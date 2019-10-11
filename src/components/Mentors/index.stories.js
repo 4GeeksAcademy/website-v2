818a41a967107e3804cbbad320fc49301094da50
plucodev <paololucanodeveloper@gmail.com>
2019-10-11 14:35:13 -0400

    Initialized Mentors component

---
+++ src/components/Mentors/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+
+import Mentors from '.';
+
+storiesOf('Mentors', module)
+    .add('Default', () => (
+        <Mentors />
+    ));
