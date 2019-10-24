1ec48b264ddb9f551a3fb5601d71d7f7245ee4af
plucodev <paololucanodeveloper@gmail.com>
2019-10-24 13:27:23 -0400

    created templates

--- src/components/Mentors/index.js
+++ src/components/Mentors/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Mentors=() => (
+const Mentors = () => (
     <div>Mentors</div>
 );
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Link} from "gatsby"
+import { Link } from 'gatsby';
 
-const Navbar=() => (
+const Navbar = () => (
 
-    <div >
-        <Link >
+    <div>
+        <Link>
             <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="" />
         </Link>
     </div>
