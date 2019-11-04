9b473a264a9bf35ec8c164aa1ed1635cfedb226e
plucodev <paololucanodeveloper@gmail.com>
2019-11-04 13:14:11 -0500

    updated alumni component

--- src/components/Alumni/index.js
+++ src/components/Alumni/index.js
@@ -3,11 +3,20 @@ import styled from 'styled-components';
 import '../../assets/css/bootstrap.min.css';
 import PropTypes from 'prop-types';
 
+const Heading = styled.h1`
+  color: red;
+  margin-left: 5px;
+  background-color: pink;
+  padding: 2px 5px;
+  border-radius: 3px;
+  
+`;
+
 const Alumni = props => (
-    <div className="container">
+    <div className="container mt-5">
         <div className="row">
             <div className="col-md-6">
-                <div className="row">{props.name}</div>
+                <div className="row"><h1>Meet </h1> <Heading>{props.name}</Heading></div>
                 <div className="row"></div>
                 <div className="row">{props.content}</div>
                 <div className="row"></div>
