552957b0e9b7ba4ceff51f93f2c1d1a94de74ee4
plucodev <paololucanodeveloper@gmail.com>
2019-11-05 14:34:28 -0500

    updated mentors component

--- src/components/Mentors/index.js
+++ src/components/Mentors/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 import '../../assets/css/bootstrap.min.css';
+import PropTypes from 'prop-types';
 
-const Mentors = () => (
+const Mentors = props => (
     <>
         <div className="container">Mentors</div>
         <div className="container">
-            <div className="row"></div>
+            <div className="row">
+                {props.mentorsArray.map((item, index) => {
+                    return (
+                        <div key={index}>{item.name}</div>
+
+                    );
+                })
+
+
+                }
+
+            </div>
 
 
 
         </div>
     </>
 );
+Mentors.propTypes = {
+    name: PropTypes.string.isRequired,
 
+};
 export default Mentors;
