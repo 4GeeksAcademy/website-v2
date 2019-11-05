57b158d78e25e033efd9a82c854e30364792c988
plucodev <paololucanodeveloper@gmail.com>
2019-11-05 14:31:04 -0500

    updated mentors component

--- src/components/Mentors/index.js
+++ src/components/Mentors/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import '../../assets/css/bootstrap.min.css';
 
 const Mentors = () => (
-    <div>Mentors</div>
+    <>
+        <div className="container">Mentors</div>
+        <div className="container">
+            <div className="row"></div>
+
+
+
+        </div>
+    </>
 );
 
 export default Mentors;
