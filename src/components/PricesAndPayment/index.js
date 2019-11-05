cb36b2fa76b347a314eac0867990fc7793d14018
plucodev <paololucanodeveloper@gmail.com>
2019-11-05 18:28:10 -0500

    updated prices component

--- src/components/PricesAndPayment/index.js
+++ src/components/PricesAndPayment/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const PricesAndPayment=() => (
-    <div>PricesAndPayment</div>
+const PricesAndPayment = () => (
+    <StaticQuery
+        query={
+            graphql`
+            query myFinacialTest{
+                financial: allFinancialsYaml {
+                    edges {
+                      node {
+                        name
+                        options {
+                          months
+                          payment
+                        }
+                        logo
+                        description
+                      }
+                    }
+                  }
+              }
+            `
+        }
+        render={data => (
+            <div>{data.financial.edges[1].node.name}</div>
+        )}
+    />
 );
 
 export default PricesAndPayment;
