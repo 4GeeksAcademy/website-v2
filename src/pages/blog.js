4ef42864897404ea6238530983248496a22978db
plucodev <paololucanodeveloper@gmail.com>
2019-11-19 16:56:21 -0500

    blog page working

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const BlogPage = () => (
+const BlogPage = ({data}) => (
     <div>
         <h1>Post One</h1>
         {
             data.allMarkdownRemark.edges.map(post => (
                 <div key={post.node.id}>
                     <h3>{post.node.frontmatter.title}</h3>
-                    <small>Posted by {post.node.author}</small>
+                    <small>Posted by {post.node.frontmatter.author} on {post.node.frontmatter.date}</small>
+                    <br />
+                    <br />
+                    <Link to={post.node.frontmatter.path}>Read More</Link>
+                    <br />
+                    <br />
                 </div>
             ))
         }
@@ -24,10 +29,12 @@ export const pageQuery = graphql`
                   title
                   path
                   name
-          date        
+                  date        
                 }
               }
             }
           }
     }
  `
+
+export default BlogPage;
