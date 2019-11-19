f5456266fefbff2f10dcaa7b9c94d12df16c8b9a
plucodev <paololucanodeveloper@gmail.com>
2019-11-19 16:48:22 -0500

    added functionalities to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,6 +4,14 @@ import Link from 'gatsby-link'
 const BlogPage = () => (
     <div>
         <h1>Post One</h1>
+        {
+            data.allMarkdownRemark.edges.map(post => (
+                <div key={post.node.id}>
+                    <h3>{post.node.frontmatter.title}</h3>
+                    <small>Posted by {post.node.author}</small>
+                </div>
+            ))
+        }
 
     </div>
 )
