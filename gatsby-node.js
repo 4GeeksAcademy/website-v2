e4b68c42c1fb0fdbe2a40b3f60805b675492a95b
plucodev <paololucanodeveloper@gmail.com>
2019-11-20 11:25:47 -0500

    updated create-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,25 +1,27 @@
 const path = require('path');
 
 exports.createPages = ({boundActionCreators, graphql}) => {
-//   const {createPage} = boundActionCreators
-//   const postTemplate = path.resolve('src/templates/blog-post.js');
-//   return graphql(`
-//     {
-//         query AllCredentialsDataYaml {
-//             allCredentialsDataYaml{
-//               edges{
-//                 node{
-//                   rating
-//                   hired_students
-//                   alumni_number
-//                   campuses
+    const {createPage} = boundActionCreators
+    const postTemplate = path.resolve('src/templates/blog-post.js');
+    return graphql(`
+    {
+        
+            allMarkdownRemark{
+                edges{
+                  node{
+                      html
+                      id
+                    frontmatter{
+                      title
+                      path
+                      author
+                      date        
+                    }
+                  }
+                }
+              }
 
-//                 }
-//               }
-//             }
-//           }
+    }
+    `)
 
-//     }
-//     `)
-
-// }
+}
