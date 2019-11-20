e7d33a17df58e90beeec6da3c236a94be13c32f7
plucodev <paololucanodeveloper@gmail.com>
2019-11-20 11:22:29 -0500

    updated create-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,6 @@
-// const path = require('path');
+const path = require('path');
 
-// exports.createPages = ({boundActionCreators, graphql}) => {
+exports.createPages = ({boundActionCreators, graphql}) => {
 //   const {createPage} = boundActionCreators
 //   const postTemplate = path.resolve('src/templates/blog-post.js');
 //   return graphql(`
