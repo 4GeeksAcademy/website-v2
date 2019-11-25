1020f3b72d46bebd33c483604b96326110c8fe14
plucodev <paololucanodeveloper@gmail.com>
2019-11-25 14:32:57 -0500

    created Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Link} from 'gatsby';
+
+const Footer = () => (
+
+    <div>
+        <Link>
+            <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="" />
+        </Link>
+    </div>
+
+);
+
+export default Footer;
