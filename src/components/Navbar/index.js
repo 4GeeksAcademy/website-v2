3d73ac15f77fa01f5bb9eed7ba9d79cc7687e214
plucodev <paololucanodeveloper@gmail.com>
2019-11-26 14:50:44 -0500

    navbar code done

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import {Link} from 'gatsby';
+import PropTypes from 'prop-types';
 
 const Navbar = ({siteTitle, menuLinks}) => (
 
--- src/global/Layout.js
+++ src/global/Layout.js
@@ -4,6 +4,7 @@ import Helmet from 'react-helmet';
 import Navbar from '../components/Navbar'
 import {StaticQuery, graphql} from 'gatsby';
 
+
 import GlobalStyle from './GlobalStyle';
 
 const Layout = ({children}) => (
