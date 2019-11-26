3653d4b38252f6f4b5bf05429123b7f66934e4d4
plucodev <paololucanodeveloper@gmail.com>
2019-11-26 14:42:54 -0500

    updated layout

--- src/global/Layout.js
+++ src/global/Layout.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
+import {StaticQuery, graphql} from 'gatsby';
 
 import GlobalStyle from './GlobalStyle';
 
-const Layout = ({ children }) => (
+const Layout = ({children}) => (
   <StaticQuery
     query={graphql`
       query SiteTitleQuery {
         site {
           siteMetadata {
             title
+              menuLinks{
+                name
+                link
+              
+            }
           }
         }
       }
