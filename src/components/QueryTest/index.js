9753c3aeb7a5a7f702b88882cf4b529a1da8fead
plucodev <paololucanodeveloper@gmail.com>
2019-12-04 15:02:19 -0500

    fixed issues with yaml

--- src/components/QueryTest/index.js
+++ src/components/QueryTest/index.js
@@ -3,20 +3,21 @@ import styled from 'styled-components';
 import {useStaticQuery, graphql} from 'gatsby';
 import {useSpring, animated} from 'react-spring'
 import range from 'lodash-es/range'
+import '../../assets/css/style.css';
 
 const interp = i => r => `translate3d(0, ${15 * Math.sin(r + (i * 2 * Math.PI) / 1.6)}px, 0)`;
 
 export default () => {
     // const classes = useStyles();
 
-    const {radians} = useSpring({
-        to: async next => {
-            while (1) await next({radians: 2 * Math.PI});
-        },
-        from: {radians: 0},
-        config: {duration: 3500},
-        reset: true
-    });
+    // const {radians} = useSpring({
+    //     to: async next => {
+    //         while (1) await next({radians: 2 * Math.PI});
+    //     },
+    //     from: {radians: 0},
+    //     config: {duration: 3500},
+    //     reset: true
+    // });
 
 
     const data = useStaticQuery(graphql`
@@ -61,18 +62,21 @@ export default () => {
 
         <div className="container ">
             {data.credentials.edges.map(i => (
-                <animated.div key={i} className="script-bf-box " style={{transform: radians.interpolate(interp(i))}}>
-                    <div className="card">
+                <div className="card">
+                    {i.node.credential}
+                </div>
+                // <animated.div key={i} className="script-bf-box " style={{transform: radians.interpolate(interp(i))}}>
+                //     <div className="card">
 
-                        <div className="card-body">
-                            <h5 className="card-title">{i.node.credentials}</h5>
-                            <p className="card-text">
-                                {i.node.cred_value}
-                            </p>
+                //         <div className="card-body">
+                //             <h5 className="card-title">{i.node.credentials}</h5>
+                //             <p className="card-text">
+                //                 {i.node.cred_value}
+                //             </p>
 
-                        </div>
-                    </div>
-                </animated.div>
+                //         </div>
+                //     </div>
+                // </animated.div>
             ))}
 
         </div>
