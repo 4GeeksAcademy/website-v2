6f6038e6637d0c4c305911838b368721522b62a9
plucodev <paololucanodeveloper@gmail.com>
2019-12-05 11:47:21 -0500

    updated chooseProgram component

--- src/components/QueryTest/index.js
+++ src/components/QueryTest/index.js
@@ -10,22 +10,22 @@ import {faGraduationCap, faTrophy, faHandshake, faBookOpen} from '@fortawesome/f
 const interp = i => r => `translate3d(0, ${15 * Math.sin(r + (i * 2 * Math.PI) / 1.6)}px, 0)`;
 
 export default () => {
-    // const classes = useStyles();
+  // const classes = useStyles();
 
-    // const {radians} = useSpring({
-    //     to: async next => {
-    //         while (1) await next({radians: 2 * Math.PI});
-    //     },
-    //     from: {radians: 0},
-    //     config: {duration: 3500},
-    //     reset: true
-    // });
-    const graduation = <FontAwesomeIcon icon={faGraduationCap} size="3x" />
-    const rating = <FontAwesomeIcon icon={faTrophy} size="3x" />
-    const campuses = <FontAwesomeIcon icon={faBookOpen} size="3x" />
-    const hired = <FontAwesomeIcon icon={faHandshake} size="3x" />
+  // const {radians} = useSpring({
+  //     to: async next => {
+  //         while (1) await next({radians: 2 * Math.PI});
+  //     },
+  //     from: {radians: 0},
+  //     config: {duration: 3500},
+  //     reset: true
+  // });
+  const graduation = <FontAwesomeIcon icon={faGraduationCap} size="3x" />
+  const rating = <FontAwesomeIcon icon={faTrophy} size="3x" />
+  const campuses = <FontAwesomeIcon icon={faBookOpen} size="3x" />
+  const hired = <FontAwesomeIcon icon={faHandshake} size="3x" />
 
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
       query myQueryTest2{
           credentials: allCredentialsDataYaml {
               edges {
@@ -63,35 +63,39 @@ export default () => {
       `)
 
 
-    return (
+  return (
 
-        <div className="container ">
-            <div className="row my-4">
+    <div className="container ">
+      <div className="row my-4">
 
-                {data.credentials.edges.map(i => (
-                    <div className="col-md-3">
-                        <div className="card-credential p-3 text-center test">
-                            <div className="icons mb-3">{graduation}</div>
-                            <div><h3>{i.node.credential}</h3></div>
-                            <div><h3>{i.node.cred_value}</h3></div>
-                        </div>
-                    </div>
-                    // <animated.div key={i} className="script-bf-box " style={{transform: radians.interpolate(interp(i))}}>
-                    //     <div className="card">
-
-                    //         <div className="card-body">
-                    //             <h5 className="card-title">{i.node.credentials}</h5>
-                    //             <p className="card-text">
-                    //                 {i.node.cred_value}
-                    //             </p>
-
-                    //         </div>
-                    //     </div>
-                    // </animated.div>
-                ))}
+        {data.credentials.edges.map((i, index) => {
+          // const props = useSpring({number: i.node.cred_value, from: {number: 0}})
+          console.log(i.node.cred_value)
+          return (
+            <div key={index} className="col-md-3">
+              <div className="card-credential p-3 text-center test">
+                <div className="icons mb-3">{graduation}</div>
+                <div><h3>{i.node.credential}</h3></div>
+                <div><h3>{i.node.cred_value}</h3></div>
+              </div>
             </div>
-        </div>
-    )
+            // <animated.div key={i} className="script-bf-box " style={{transform: radians.interpolate(interp(i))}}>
+            //     <div className="card">
+
+            //         <div className="card-body">
+            //             <h5 className="card-title">{i.node.credentials}</h5>
+            //             <p className="card-text">
+            //                 {i.node.cred_value}
+            //             </p>
+
+            //         </div>
+            //     </div>
+            // </animated.div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 
