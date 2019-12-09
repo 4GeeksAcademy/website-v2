5b7e8b97b592ed6089d316b1080fe7ff54055719
plucodev <paololucanodeveloper@gmail.com>
2019-12-09 17:41:28 -0500

    fixed issues footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,10 +8,18 @@ const Footer = () => (
         <div className="jumbotron jumbotron-fluid footer mb-0">
             <div className="container">
                 <div className="row">
-                    <div className="col-md-3"><div className="row">CONTACT</div></div>
-                    <div className="col-md-3"><div className="row">COMPANY</div></div>
-                    <div className="col-md-3"><div className="row">LOCATIONS</div></div>
-                    <div className="col-md-3"><div className="row">FOLLOW</div></div>
+                    <div className="col-md-3">
+                        <div className="row footer-title text-white">CONTACT</div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="row footer-title text-white">COMPANY</div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="row footer-title text-white">LOCATIONS</div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="row footer-title text-white">FOLLOW</div>
+                    </div>
                 </div>
             </div>
         </div>
