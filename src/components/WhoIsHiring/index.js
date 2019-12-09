bc9180ea85ee77307c5cf800bae01a79b259a1be
plucodev <paololucanodeveloper@gmail.com>
2019-12-09 13:37:11 -0500

    updated whoishiring component

--- src/components/WhoIsHiring/index.js
+++ src/components/WhoIsHiring/index.js
@@ -6,7 +6,7 @@ const WhoIsHiring = () => (
         <div className="row who-one">
             <div class="col-md-6 offset-md-3 program-title text-white text-center">
 
-                <div className="row justify-content-center mt-5 who-title">WHY 4 GEEKS ?</div>
+                <div className="row justify-content-center mt-5 who-title">WHO IS HIRING OUR STUDENTS ?</div>
                 <div className="row justify-content-center mb-3">
                     <div className=" program-divider"></div>
                 </div>
