d0ce99771cfa0765455c167944f7abce9ca81786
plucodev <paololucanodeveloper@gmail.com>
2019-12-11 13:22:04 -0500

    updated index.js

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -18,15 +18,20 @@ export const H3 = styled.h3`
 export const H4 = styled.h4`
   font-size: 16px;
 `;
-export const Sep = styled.div`
+export const Separator = styled.div`
   border-bottom: ${props => props.primary ? "1px solid black" : "1px solid black"};
+`
+export const Paragraph = styled.div`
+  text-align: center;
+  font-size: 12px;
+  color: ${props => props.primary ? `$yellow` : "black"};
 `
 export const Title = props => {
   return (
     <>
       <div className="row px-5">{props.style == "light" ? <H2 primary>{props.title}</H2> : <H2>{props.title}</H2>}</div>
-      <div className="row">{props.style == "light" ? <Sep primary>{props.title}</Sep> : <Sep>{props.title}</Sep>}</div>
-      <div className="row"></div>
+      <div className="row">{props.style == "light" ? <Separator primary>{props.title}</Separator> : <Separator>{props.title}</Separator>}</div>
+      <div className="row">{props.style == "light" ? <Paragraph primary>{props.title}</Paragraph> : <Paragraph>{props.title}</Paragraph>}</div>
     </>
   )
 }
