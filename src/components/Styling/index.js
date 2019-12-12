874decfedbbc7debd02988cc8377ce6541947478
plucodev <paololucanodeveloper@gmail.com>
2019-12-12 13:52:12 -0500

    updated styling component

--- src/components/Styling/index.js
+++ src/components/Styling/index.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
-
 
 export const Colors = {
     blue: "#0097CE",
@@ -11,7 +8,6 @@ export const Colors = {
     yellow: "#E6BA1F",
     black: "#000000",
     white: "#FFFFFF"
-
 }
 
 
