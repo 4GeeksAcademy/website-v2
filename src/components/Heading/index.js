10a3fc4b02bf4b043730fdca71b61df3edd10fef
plucodev <paololucanodeveloper@gmail.com>
2019-12-12 13:51:38 -0500

    creating styling component

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import '../../assets/css/_colors.scss'
 
 export const H1 = styled.h1`
   font-size: 12px;
---
+++ src/components/Styling/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+
+export const Colors = {
+    blue: "#0097CE",
+    lightBlue: "#BBEAFC",
+    gray: "#DEDEDE",
+    lightGray: "#F9F9F9",
+    yellow: "#E6BA1F",
+    black: "#000000",
+    white: "#FFFFFF"
+
+}
+
+
