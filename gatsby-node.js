111b3994e31a8579872463aa6691929114fd594b
plucodev <paololucanodeveloper@gmail.com>
2019-12-12 10:24:25 -0500

    fixed issue gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
-exports.createPages = ({boundActionCreators, graphql}) => {
-    const {createPage} = boundActionCreators
-    const postTemplate = path.resolve('src/templates/blog-post.js');
-    return graphql(`
+exports.createPages = ({actions, graphql}) => {
+  const {createPage} = actions
+  const postTemplate = path.resolve('src/templates/blog-post.js');
+  return graphql(`
     {
         
             allMarkdownRemark{
@@ -23,15 +23,15 @@ exports.createPages = ({boundActionCreators, graphql}) => {
 
     }
     `).then(res => {
-        if (res.errors) {
-            return Promise.reject(res.errors)
-        }
-        res.data.allMarkdownRemark.edges.forEach(({node}) => {
-            createPage({
-                path: node.frontmatter.path,
-                component: postTemplate
-            })
-        })
+    if (res.errors) {
+      return Promise.reject(res.errors)
+    }
+    res.data.allMarkdownRemark.edges.forEach(({node}) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: postTemplate
+      })
     })
+  })
 
 }
