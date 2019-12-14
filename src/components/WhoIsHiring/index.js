56ae28e76b7a5dd3d0e39c9c200c554e959680a4
plucodev <paololucanodeveloper@gmail.com>
2019-12-13 19:12:55 -0500

    fixed issues

--- src/components/WhoIsHiring/index.js
+++ src/components/WhoIsHiring/index.js
@@ -17,27 +17,50 @@ const WhoIsHiring = () => {
           }
         }
       `)
-    let content = [];
-    console.log("content", content)
-    data.who.edges[0].node.partners.map((partner, i) => {
-        if ((i + 1) % 4 == 0) {
-            content.push(
-                <div className="row">
-                    <div key={i} className="col-md-3 test">
-                        <img src={partner.image} width="128px" alt={partner.name} />
-                    </div>
-                </div>
-            )
-        } else {
-            content.push(<div key={i} className="col-md-3"><img src={partner.image} width="128px" alt={partner.name} /></div>);
-        }
-    });
-    return (
-        <div>
-            {content}
-        </div>
-    );
 
+    return (
+        <Container>
+            <div className="row who-one mb-3">
+                <div class="col-md-6 offset-md-3 program-title text-white text-center">
+                    <div className="row justify-content-center mt-5 who-title">WHO IS HIRING OUR STUDENTS ?</div>
+                    <div className="row justify-content-center mb-3">
+                        <div className=" program-divider"></div>
+                    </div>
+                    <div className="row justify-content-center who-lead ">
+                        Our hiring partners are growing everyday and our students have been hired from dozens of comapnies in the U.S. and worldwide.
+                </div>
+                </div>
+            </div>
+            <div className="row who-two">
+                <div class="col-md-12  program-title text-white text-center">
+                    <div className="row  mt-5 who-boxes justify-content-center">
+                        <div className="col-md-2 mr-3"></div>
+                        <div className="col-md-2 mr-3"></div>
+                        <div className="col-md-2 mr-3"></div>
+                        <div className="col-md-2"></div>
+                    </div>
+                    <div className="row justify-content-center mt-3 who-boxes">
+                        <div className="col-md-2 mr-3"></div>
+                        <div className="col-md-2 mr-3"></div>
+                        <div className="col-md-2 mr-3"></div>
+                        <div className="col-md-2"></div>
+                    </div>
+                    <div className="row justify-content-center mt-3 who-boxes">
+                        <div className="col-md-2 mr-3"></div>
+                        <div className="col-md-2 mr-3"></div>
+                        <div className="col-md-2 mr-3"></div>
+                        <div className="col-md-2"></div>
+                    </div>
+                    <div className="row justify-content-center mb1 who-title-small">
+                        438 COMPANIES
+                </div>
+                    <div className="row justify-content-center who-lead-small ">
+                        Review our latest hirings
+                </div>
+                </div>
+            </div>
+        </Container>
+    )
 };
 
 export default WhoIsHiring;
