3943f9e7481b55583b64f1ed9609efaa30f2f421
plucodev <paololucanodeveloper@gmail.com>
2019-12-16 16:23:47 -0500

    fixed Card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import {Colors} from '../../components/Styling'
 
-const Card = () => (
-    <div>Card</div>
-);
-
-export default Card;
+export const Card = styled.div`
+    width: ${props => props.width};
+    height: ${props => props.height};
+    background: ${props => props.color === "blue"
+        ?
+        `${Colors.blue}`
+        : props.color === "grey"
+            ? `${Colors.lightGray}`
+            : `${Colors.white}`
+    };
+    border-radius: ${props => props.borders};
+`;
+Card.propTypes = {
+    color: PropTypes.string,
+    width: PropTypes.string,
+    height: PropTypes.string,
+    borders: PropTypes.string,
+}
+Card.defaultProps = {
+    borders: '1.25rem',
+};
