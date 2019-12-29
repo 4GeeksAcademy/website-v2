abc238dd47ab40baeaaff48eae112f1e8dd5d28f
plucodev <paololucanodeveloper@gmail.com>
2019-12-29 12:06:10 -0500

    installed react-select package and started choose program

--- src/components/ChooseProgram/index.js
+++ src/components/ChooseProgram/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 
-const options = [
-    {value: 'chocolate', label: 'Chocolate'},
-    {value: 'strawberry', label: 'Strawberry'},
-    {value: 'vanilla', label: 'Vanilla'},
-];
-const ChooseProgram = () => (
-    <div>ChooseProgram</div>
-);
+const ChooseProgram = () => {
+    const [useOption, setOption] = useState([
+        {value: 'chocolate', label: 'Chocolate'},
+        {value: 'strawberry', label: 'Strawberry'},
+        {value: 'vanilla', label: 'Vanilla'},
+    ])
+    return (
+        <div>ChooseProgram</div>
+    )
+};
 
 export default ChooseProgram;
