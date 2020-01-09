2259f1259791d092b28bd60c2d63ffb0762152df
plucodev <paololucanodeveloper@gmail.com>
2020-01-09 14:51:18 -0500

    updated geeksvsothers with staticQuery

--- src/components/GeeksVsOthers/index.js
+++ src/components/GeeksVsOthers/index.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
+import {useStaticQuery, graphql} from 'gatsby';
 import {Row, Container, Column} from '../Sections'
 import {H5} from '../Heading'
 
-const GeeksVsOthers = () => (
-    <Row>
-        <Column
-            size="10"
-        >
-            <Row>
-                <Column size="6">
-                    <H5 fontSize="12px">FEATURED</H5>
-                </Column>
-                <Column size="3"><H5 fontSize="12px">AT 4GEEKS</H5></Column>
-                <Column size="3"><H5 fontSize="12px">INDUSTRY AVERAGE</H5></Column>
-            </Row>
-        </Column>
-    </Row>
-);
+const GeeksVsOthers = () => {
+    const data = useStaticQuery(graphql`
+      query my4GeeksDataQuery{
+        allGeeksDataYaml {
+            edges {
+              node {
+                _4Geeks {
+                  features
+                  industry_average
+                  icon
+                  tooltip
+                  at4_Geeks
+                }
+              }
+            }
+          }
+        }
+      `)
+    return (
+        <Row>
+            <Column
+                size="10"
+            >
+                <Row>
+                    <Column size="6">
+                        <H5 fontSize="12px">FEATURED</H5>
+                    </Column>
+                    <Column size="3"><H5 fontSize="12px">AT 4GEEKS</H5></Column>
+                    <Column size="3"><H5 fontSize="12px">INDUSTRY AVERAGE</H5></Column>
+                </Row>
+                {}
+            </Column>
+        </Row>
+    )
+};
 
 export default GeeksVsOthers;
