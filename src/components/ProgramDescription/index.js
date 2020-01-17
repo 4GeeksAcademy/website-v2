eeb51133f84859072350aa06c08d626bfd535ca5
plucodev <paololucanodeveloper@gmail.com>
2020-01-16 22:17:24 -0500

    Updated program description

--- src/components/ProgramDescription/index.js
+++ src/components/ProgramDescription/index.js
@@ -29,10 +29,10 @@ const ProgramDescription = () => (
         <Header>
             <ul className="nav  nav-fill">
                 <li className="nav-item">
-                    <a className="nav-link active" href="#">PREWORK</a>
+                    <a className="nav-link active" id="home-tab" data-toggle="tab" href="#homes" role="tab" aria-controls="home" aria-selected="true" >PREWORK</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#">MODULE 1</a>
+                    <a className="nav-link" id="profile-tab" data-toggle="tab" role="tab" aria-controls="profile" aria-selected="true" href="#profile">MODULE 1</a>
                 </li>
                 <li className="nav-item">
                     <a className="nav-link" href="#">MODULE 2</a>
@@ -43,16 +43,24 @@ const ProgramDescription = () => (
             </ul>
         </Header>
         <Body>
-            <H5 fontSize="20px">PREWORK</H5>
-            <Paragraph primary margin="35px 0 15px" fontSize="14px" lineHeight="18px">
-                Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam id dolor id nibh ultricies vehicula ut id elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.
-                Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor. Aenean lacinia bibendum nulla sed consectetur. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+            <div className="tab-content" id="myTabContent">
+                <div className="tab-pane fade show active" id="homes" role="tabpanel" aria-labelledby="home-tab">
+                    <H5 fontSize="20px">PREWORK</H5>
+                    <Paragraph primary margin="35px 0 15px" fontSize="14px" lineHeight="18px">
+                        Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam id dolor id nibh ultricies vehicula ut id elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.
+                        Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor. Aenean lacinia bibendum nulla sed consectetur. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
             </Paragraph>
-            <Paragraph primary fontSize="14px" lineHeight="18px">
-                Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam id dolor id nibh ultricies vehicula ut id elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.
-                Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor. Aenean lacinia bibendum nulla sed consectetur. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam id dolor id nibh ultricies vehicula ut id elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.
+                    <Paragraph primary fontSize="14px" lineHeight="18px">
+                        Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam id dolor id nibh ultricies vehicula ut id elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.
+                        Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor. Aenean lacinia bibendum nulla sed consectetur. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam id dolor id nibh ultricies vehicula ut id elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.
             </Paragraph>
-            <Button color={Colors.blue} textColor={Colors.white} margin="15px 0">DOWNLOAD SYLLABUS</Button>
+                    <Button color={Colors.blue} textColor={Colors.white} margin="15px 0">DOWNLOAD SYLLABUS</Button>
+                </div>
+                <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">Paolo</div>
+                <div className="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+                <div className="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+            </div>
+
         </Body>
 
         {/* </Row> */}
