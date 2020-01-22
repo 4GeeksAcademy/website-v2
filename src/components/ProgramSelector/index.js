f31b27a3604461a2c50115e551262ffa9f7add9d
plucodev <paololucanodeveloper@gmail.com>
2020-01-22 17:38:47 -0500

    updated Program Selector component

--- src/components/ProgramSelector/index.js
+++ src/components/ProgramSelector/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+import {Row, Column} from '../Sections';
 
-const ProgramSelector = () => (
-    <div>Badges</div>
-);
+const ProgramSelector = () => {
+    return (
+        <Row>
+            <Column size="12"></Column>
+        </Row>
+
+    )
+};
 
 export default ProgramSelector;
