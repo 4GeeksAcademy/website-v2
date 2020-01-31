7c37b54fc0bbe20df877e2e4fd184ec52a6aa895
plucodev <paololucanodeveloper@gmail.com>
2020-01-31 16:11:25 -0500

    updated Footer responsiveness

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -12,19 +12,19 @@ const Footer = () => (
                 <Column size="2"></Column>
                 <Column size="7">
                     <Row align="center">
-                        <Column size="3">
+                        <Column size="3" customRespSize respSize="6" margin="5px 0">
                             <Row marginBottom="20px"><H5 fontSize="16px" color={Colors.gray}>CONTACT</H5></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.gray}>Monday to Friday</Paragraph></Row>
                             <Row><Paragraph fontSize="12px" color={Colors.gray}>9::00am - 6:00pm </Paragraph></Row>
                         </Column>
-                        <Column size="3">
+                        <Column size="3" customRespSize respSize="6" margin="5px 0">
                             <Row marginBottom="20px"><H5 fontSize="16px" color={Colors.gray}>COMPANY</H5></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Program</Paragraph></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>The Academy</Paragraph></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Partners</Paragraph></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Pricing</Paragraph></Row>
                         </Column>
-                        <Column size="3">
+                        <Column size="3" customRespSize respSize="6" margin="5px 0">
                             <Row marginBottom="20px"><H5 fontSize="16px" color={Colors.gray}>LOCATIONS</H5></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Santiago de Chile</Paragraph></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Miami, Brickell</Paragraph></Row>
@@ -32,7 +32,7 @@ const Footer = () => (
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Maracaibo, Vzla</Paragraph></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Madrid, Spain</Paragraph></Row>
                         </Column>
-                        <Column size="3">
+                        <Column size="3" customRespSize respSize="6" margin="5px 0">
                             <Row marginBottom="20px"> <H5 fontSize="16px" color={Colors.gray}>FOLLOW</H5></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Github</Paragraph></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Facebook</Paragraph></Row>
