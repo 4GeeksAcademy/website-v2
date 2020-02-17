7a9f47d536f19e96c2417959891c57b2a4181712
plucodev <paololucanodeveloper@gmail.com>
2020-02-17 16:09:00 -0500

    removed payment from Choose Program component

--- src/components/ChooseProgram/index.js
+++ src/components/ChooseProgram/index.js
@@ -20,19 +20,17 @@ const ChooseProgram = () => {
         <>
             <Card index="1" shadow width="230px" padding={toggle === false ? "0px" : "0 0 10px 0"}>
                 <Row marginRight="0" marginLeft="0" align="center"><Button onClick={() => setToggle(!toggle)} color={Colors.blue} textColor={Colors.white}>CHOOSE YOUR PROGRAM</Button></Row>
-
                 {toggle == true
                     ?
                     <>
                         <Row marginBottom="5px" marginTop="5px" marginRight="0" marginLeft="0" align="center"><Button width="90%" color={Colors.lightGray} textColor={Colors.gray} borderRadius=".25rem">Program</Button></Row>
                         <Row marginBottom="5px" marginRight="0" marginLeft="0" align="center"><Button width="90%" color={Colors.lightGray} textColor={Colors.gray} borderRadius=".25rem">Location</Button></Row>
-                        <Row marginBottom="5px" marginRight="0" marginLeft="0" align="center"><Button width="90%" color={Colors.lightGray} textColor={Colors.gray} borderRadius=".25rem">Payment</Button></Row>
+                        {/* <Row marginBottom="5px" marginRight="0" marginLeft="0" align="center"><Button width="90%" color={Colors.lightGray} textColor={Colors.gray} borderRadius=".25rem">Payment</Button></Row> */}
                         <Row marginRight="0" marginLeft="0" align="center"><Button width="90%" onClick={() => setToggle(!toggle)} color={Colors.red} textColor={Colors.white}>APPLY NOW</Button></Row>
                     </>
                     :
                     null
                 }
-
             </Card>
 
             {/* <Button color={Colors.blue}>
