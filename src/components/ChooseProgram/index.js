165b892b5d2841db52af477abdb685c5fa8ed105
plucodev <paololucanodeveloper@gmail.com>
2020-02-16 22:09:55 -0500

    updated chooseprogram

--- src/components/ChooseProgram/index.js
+++ src/components/ChooseProgram/index.js
@@ -1,10 +1,13 @@
 import React, {useEffect, useState, useContext} from 'react';
 import Dropdown from 'react-bootstrap/Dropdown'
 import styled from 'styled-components';
-import {SessionContext} from '../../session';
+// import {SessionContext} from '../../session';
+import {Button, Colors} from '../Styling';
+import {Card} from '../Card'
 
 const ChooseProgram = () => {
-    const {session, setSession} = useContext(SessionContext)
+    // const {session, setSession} = useContext(SessionContext)
+    const [toggle, setToggle] = useState(false)
     const [option, setOption] = useState([
         {value: 'chocolate', label: 'Chocolate'},
         {value: 'strawberry', label: 'Strawberry'},
@@ -12,66 +15,87 @@ const ChooseProgram = () => {
     ])
 
     return (
-        <Dropdown>
-            <Dropdown.Toggle variant="light" id="dropdown-basic">
-                Choose Your Program
+        <>
+            <Card shadow width="200px" >
+                <Button onClick={() => setToggle(!toggle)} color={Colors.blue} textColor={Colors.white}>Choose Program</Button>
+                {toggle == true
+                    ?
+                    <>
+                        <div >ciao</div>
+                        <div>ciao</div>
+                        <div>ciao</div>
+                    </>
+                    :
+                    null
+                }
+
+            </Card>
+
+            {/* <Button color={Colors.blue}>
+                ChooseProgram
+            </Button>
+            {toggle == "true" && <div>test</div>}
+            <Dropdown>
+                <Dropdown.Toggle variant="light" id="dropdown-basic">
+                    Choose Your Program
             </Dropdown.Toggle>
 
-            <Dropdown.Menu>
-                <Dropdown.Item >
-                    <Dropdown>
-                        <Dropdown.Toggle variant="light" id="dropdown-basic">
-                            Program
+                <Dropdown.Menu>
+                    <Dropdown.Item >
+                        <Dropdown>
+                            <Dropdown.Toggle variant="light" id="dropdown-basic">
+                                Program
             </Dropdown.Toggle>
 
-                        {option.map((item, index) => {
-                            return (
-                                <Dropdown.Menu key={index}>
-                                    <Dropdown.Item href="#/action-1">{item.value}</Dropdown.Item>
-                                    <Dropdown.Divider />
-                                    <Dropdown.Item href="#/action-2">{item.value}</Dropdown.Item>
-                                    <Dropdown.Divider />
-                                    <Dropdown.Item href="#/action-3">{item.value}</Dropdown.Item>
-                                </Dropdown.Menu>)
-                        })}
-                    </Dropdown>
-                </Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item >
-                    <Dropdown>
-                        <Dropdown.Toggle variant="light" id="dropdown-basic">
-                            Location
+                            {option.map((item, index) => {
+                                return (
+                                    <Dropdown.Menu key={index}>
+                                        <Dropdown.Item href="#/action-1">{item.value}</Dropdown.Item>
+                                        <Dropdown.Divider />
+                                        <Dropdown.Item href="#/action-2">{item.value}</Dropdown.Item>
+                                        <Dropdown.Divider />
+                                        <Dropdown.Item href="#/action-3">{item.value}</Dropdown.Item>
+                                    </Dropdown.Menu>)
+                            })}
+                        </Dropdown>
+                    </Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item >
+                        <Dropdown>
+                            <Dropdown.Toggle variant="light" id="dropdown-basic">
+                                Location
             </Dropdown.Toggle>
 
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Program</Dropdown.Item>
-                            <Dropdown.Divider />
-                            <Dropdown.Item href="#/action-2">Location</Dropdown.Item>
-                            <Dropdown.Divider />
-                            <Dropdown.Item href="#/action-3">Payment</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item href="#/action-3">
-                    <Dropdown>
-                        <Dropdown.Toggle variant="light" id="dropdown-basic">
-                            Payment
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Program</Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item href="#/action-2">Location</Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item href="#/action-3">Payment</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item href="#/action-3">
+                        <Dropdown>
+                            <Dropdown.Toggle variant="light" id="dropdown-basic">
+                                Payment
             </Dropdown.Toggle>
 
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Program</Dropdown.Item>
-                            <Dropdown.Divider />
-                            <Dropdown.Item href="#/action-2">Location</Dropdown.Item>
-                            <Dropdown.Divider />
-                            <Dropdown.Item href="#/action-3">Payment</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Dropdown.Item>
-                <div className="btn btn-block btn-sm btn-warning rounded-pill mt-1">APPLY NOW</div>
-            </Dropdown.Menu>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Program</Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item href="#/action-2">Location</Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item href="#/action-3">Payment</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Dropdown.Item>
+                    <div className="btn btn-block btn-sm btn-warning rounded-pill mt-1">APPLY NOW</div>
+                </Dropdown.Menu>
 
-        </Dropdown>
+            </Dropdown> */}
+        </>
     )
 };
 
