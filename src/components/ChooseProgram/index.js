bad073f8cee658b65032dbaba0893c15d78df5eb
plucodev <paololucanodeveloper@gmail.com>
2020-02-17 16:28:50 -0500

    updated choose program

--- src/components/ChooseProgram/index.js
+++ src/components/ChooseProgram/index.js
@@ -1,20 +1,13 @@
 import React, {useEffect, useState, useContext} from 'react';
-import Dropdown from 'react-bootstrap/Dropdown'
-import styled from 'styled-components';
-// import {SessionContext} from '../../session';
+import {SessionContext} from '../../session';
 import {Button, Colors} from '../Styling';
 import {Card} from '../Card'
 import {Row, Column} from '../Sections'
 
 const ChooseProgram = () => {
-    // const {session, setSession} = useContext(SessionContext)
+    const {session, setSession} = useContext(SessionContext)
     const [toggle, setToggle] = useState(false)
     const [program, setProgram] = useState(false)
-    const [option, setOption] = useState([
-        {value: 'chocolate', label: 'Chocolate'},
-        {value: 'strawberry', label: 'Strawberry'},
-        {value: 'vanilla', label: 'Vanilla'},
-    ])
 
     return (
         <>
@@ -23,8 +16,14 @@ const ChooseProgram = () => {
                 {toggle == true
                     ?
                     <>
-                        <Row marginBottom="5px" marginTop="5px" marginRight="0" marginLeft="0" align="center"><Button width="90%" color={Colors.lightGray} textColor={Colors.gray} borderRadius=".25rem">Program</Button></Row>
-                        <Row marginBottom="5px" marginRight="0" marginLeft="0" align="center"><Button width="90%" color={Colors.lightGray} textColor={Colors.gray} borderRadius=".25rem">Location</Button></Row>
+                        <Row marginBottom="5px" marginTop="5px" marginRight="0" marginLeft="0" align="center">
+                            <Button width="90%" color={Colors.lightGray} textColor={Colors.gray} borderRadius=".25rem">
+                                <>
+                                    Select Program
+                                </>
+                            </Button>
+                        </Row>
+                        <Row marginBottom="5px" marginRight="0" marginLeft="0" align="center"><Button width="90%" color={Colors.lightGray} textColor={Colors.gray} borderRadius=".25rem">Select Location</Button></Row>
                         {/* <Row marginBottom="5px" marginRight="0" marginLeft="0" align="center"><Button width="90%" color={Colors.lightGray} textColor={Colors.gray} borderRadius=".25rem">Payment</Button></Row> */}
                         <Row marginRight="0" marginLeft="0" align="center"><Button width="90%" onClick={() => setToggle(!toggle)} color={Colors.red} textColor={Colors.white}>APPLY NOW</Button></Row>
                     </>
@@ -33,70 +32,6 @@ const ChooseProgram = () => {
                 }
             </Card>
 
-            {/* <Button color={Colors.blue}>
-                ChooseProgram
-            </Button>
-            {toggle == "true" && <div>test</div>}
-            <Dropdown>
-                <Dropdown.Toggle variant="light" id="dropdown-basic">
-                    Choose Your Program
-            </Dropdown.Toggle>
-
-                <Dropdown.Menu>
-                    <Dropdown.Item >
-                        <Dropdown>
-                            <Dropdown.Toggle variant="light" id="dropdown-basic">
-                                Program
-            </Dropdown.Toggle>
-
-                            {option.map((item, index) => {
-                                return (
-                                    <Dropdown.Menu key={index}>
-                                        <Dropdown.Item href="#/action-1">{item.value}</Dropdown.Item>
-                                        <Dropdown.Divider />
-                                        <Dropdown.Item href="#/action-2">{item.value}</Dropdown.Item>
-                                        <Dropdown.Divider />
-                                        <Dropdown.Item href="#/action-3">{item.value}</Dropdown.Item>
-                                    </Dropdown.Menu>)
-                            })}
-                        </Dropdown>
-                    </Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item >
-                        <Dropdown>
-                            <Dropdown.Toggle variant="light" id="dropdown-basic">
-                                Location
-            </Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                                <Dropdown.Item href="#/action-1">Program</Dropdown.Item>
-                                <Dropdown.Divider />
-                                <Dropdown.Item href="#/action-2">Location</Dropdown.Item>
-                                <Dropdown.Divider />
-                                <Dropdown.Item href="#/action-3">Payment</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item href="#/action-3">
-                        <Dropdown>
-                            <Dropdown.Toggle variant="light" id="dropdown-basic">
-                                Payment
-            </Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                                <Dropdown.Item href="#/action-1">Program</Dropdown.Item>
-                                <Dropdown.Divider />
-                                <Dropdown.Item href="#/action-2">Location</Dropdown.Item>
-                                <Dropdown.Divider />
-                                <Dropdown.Item href="#/action-3">Payment</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </Dropdown.Item>
-                    <div className="btn btn-block btn-sm btn-warning rounded-pill mt-1">APPLY NOW</div>
-                </Dropdown.Menu>
-
-            </Dropdown> */}
         </>
     )
 };
