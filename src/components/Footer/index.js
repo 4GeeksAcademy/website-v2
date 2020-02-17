adf8b0bc035b447d27f2e6fc3713889750b986a0
plucodev <paololucanodeveloper@gmail.com>
2020-02-17 16:08:16 -0500

    added images to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,34 +6,39 @@ import {H5, Separator, Paragraph} from '../Heading'
 
 const Footer = () => (
     <>
-        <Container width="fluid" height="300px" color={Colors.black}>
+        <Container width="fluid" height="350px" color={Colors.black}>
             <Divider height="20%" />
             <Row height="60%">
                 <Column size="2"></Column>
-                <Column size="7">
+                <Column size="8">
+
                     <Row align="center">
-                        <Column size="3" customRespSize respSize="6" margin="5px 0">
-                            <Row marginBottom="20px"><H5 fontSize="16px" color={Colors.gray}>CONTACT</H5></Row>
+                        <Column size="3" margin="0 0 20px 0">
+                            <Row><H5 fontSize="16px" color={Colors.gray}>CONTACT</H5></Row>
+                            <Row marginBottom="10px"><Separator primary></Separator></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.gray}>Monday to Friday</Paragraph></Row>
                             <Row><Paragraph fontSize="12px" color={Colors.gray}>9::00am - 6:00pm </Paragraph></Row>
                         </Column>
-                        <Column size="3" customRespSize respSize="6" margin="5px 0">
-                            <Row marginBottom="20px"><H5 fontSize="16px" color={Colors.gray}>COMPANY</H5></Row>
+                        <Column size="3" margin="0 0 20px 0">
+                            <Row><H5 fontSize="16px" color={Colors.gray}>COMPANY</H5></Row>
+                            <Row marginBottom="10px"><Separator primary></Separator></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Program</Paragraph></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>The Academy</Paragraph></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Partners</Paragraph></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Pricing</Paragraph></Row>
                         </Column>
-                        <Column size="3" customRespSize respSize="6" margin="5px 0">
-                            <Row marginBottom="20px"><H5 fontSize="16px" color={Colors.gray}>LOCATIONS</H5></Row>
+                        <Column size="3" margin="0 0 20px 0">
+                            <Row ><H5 fontSize="16px" color={Colors.gray}>LOCATIONS</H5></Row>
+                            <Row marginBottom="10px"><Separator primary></Separator></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Santiago de Chile</Paragraph></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Miami</Paragraph></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Caracas</Paragraph></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Maracaibo</Paragraph></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Madrid</Paragraph></Row>
                         </Column>
-                        <Column size="3" customRespSize respSize="6" margin="5px 0">
-                            <Row marginBottom="20px"> <H5 fontSize="16px" color={Colors.gray}>FOLLOW</H5></Row>
+                        <Column size="3" margin="0 0 20px 0">
+                            <Row> <H5 fontSize="16px" color={Colors.gray}>FOLLOW</H5></Row>
+                            <Row marginBottom="10px"><Separator primary></Separator></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Github</Paragraph></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Facebook</Paragraph></Row>
                             <Row marginBottom="5px"><Paragraph fontSize="12px" color={Colors.white}>Instagram</Paragraph></Row>
@@ -45,19 +50,20 @@ const Footer = () => (
             </Row>
             <Row height="20%">
                 <Column size="2"></Column>
-                <Column size="7">
+                <Column size="8">
                     <Row align="center">
-                        <Column size="3">
-
+                        <Column size="9" margin="5px 0" customRespSize respSize="6">
+                            <Paragraph fontSize="12px" color={Colors.gray}>@ 4Geeks Academy LLC 2019 </Paragraph>
                         </Column>
-                        <Column size="3">
-
-                        </Column>
-                        <Column size="3">
-
-                        </Column>
-                        <Column size="3">
-                            {/* <RoundImage url="../images/bitcoin.png" width="100px" bsize="cover" height="100px"></RoundImage> */}
+                        <Column size="3" margin="5px 0" customRespSize respSize="6">
+                            <Row>
+                                <Column size="6" customRespSize respSize="6">
+                                    <RoundImage url="../images/bitcoin.png" height="12px" backgroundColor="transparent" position="center" bsize="contain" />
+                                </Column>
+                                <Column size="6" customRespSize respSize="6">
+                                    <RoundImage url="../images/ethereum.png" height="14px" backgroundColor="transparent" position="center" bsize="contain" />
+                                </Column>
+                            </Row>
                         </Column>
                     </Row>
                 </Column>
