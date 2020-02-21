4f0a7cd49b621d006312ae698e6814236b224eeb
plucodev <paololucanodeveloper@gmail.com>
2020-02-21 16:05:16 -0500

    fixed all errors

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
 import Layout from '../global/Layout';
-
 const NotFoundPage = () => (
-  <Layout>
+  <Layout
+    seo={{
+      slug: '404',
+      title: '4Geeks Academy - Page not found',
+      description: '4Geeks Academy - Page not found',
+      image: "",
+      keywords: []
+    }}
+    context={{
+      lang: "us"
+    }}
+  >
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 );
-
 export default NotFoundPage;
