9529ee21ebb35cd7c81fad2f3b4a11e9f64ee486
plucodev <paololucanodeveloper@gmail.com>
2020-03-02 18:59:19 -0500

    updated program selector dropdown

--- src/components/ChooseProgram/index.js
+++ src/components/ChooseProgram/index.js
@@ -13,7 +13,7 @@ const ChooseProgram = () => {
     const [toggles, setToggles] = useState(false)
     const [toggleProg, setToggleProg] = useState(false)
     const [city, setCity] = useState(session.location)
-    const [program, setProgram] = useState("Part-Time")
+    const [program, setProgram] = useState("Select The Program")
 
     const data = useStaticQuery(graphql`
       query myQueryChoose{
@@ -37,8 +37,7 @@ const ChooseProgram = () => {
         }
       `)
     let locArray = data.loc.edges;
-    let locPrograms = ["Part-Time", "Full-Time"]
-    console.log(locArray)
+    let locPrograms = ["16 Weeks", "9 Weeks"]
     return (
         <>
             <Card
@@ -50,36 +49,28 @@ const ChooseProgram = () => {
 
                 shadow width="230px"
                 padding={toggle === false ? "0px" : "0 0 10px 0"}>
-                <Row marginRight="0" marginLeft="0" align="center"><Button width="100%" onClick={() => setToggle(!toggle)} color={Colors.blue} textColor={Colors.white}>CHOOSE YOUR PROGRAM</Button></Row>
+                <Row marginRight="0" marginLeft="0" align="center"><Button width="100%" onClick={() => setToggle(!toggle)} color={Colors.blue} textColor={Colors.white}>{toggle === true ? "Close" : "CHOOSE YOUR PROGRAM"}</Button></Row>
                 {toggle == true
                     ?
-                    <>
-                        <Row marginBottom="5px" marginRight="0" marginLeft="0" align="center">
-                            <Button width="95%" color={Colors.lightGray} textColor={Colors.gray} borderRadius=".25rem">
-                                <Card index="1" borders=".25rem" shadow width="100%" padding={toggles === false && "0px"}>
-                                    {toggles == true ? null : <Row align="center"><Button borderRadius=".25rem" onClick={() => setToggles(!toggles)} textColor={Colors.gray}>{`${program}`}</Button></Row>}
-                                    {toggles == true
-                                        ?
-                                        <>
-                                            {locPrograms.map((item, index) => {
-                                                return (
-                                                    <Row height="25px" onClick={() => {setProgram(item), setToggles(!toggles)}} backgroundHover={Colors.lightBlue} colorHover={Colors.white} key={index} marginBottom="5px" marginTop="5px" marginRight="0" marginLeft="0" align="around">
-                                                        <Column size="12" alignSelf="center"><Paragraph fontSize="16px" color={Colors.gray} >{item}</Paragraph></Column>
-                                                    </Row>
-                                                )
-                                            })}
-                                        </>
-                                        :
-                                        null
-                                    }
-                                </Card>
-                            </Button></Row>
-                        <Row marginRight="0" marginLeft="0" align="center">
-                            <Link to={program === "Part-Time" ? "/course/full-stack-web-development-bootcamp-part-time" : program === "Full-Time" ? "/course/full-stack-web-development-bootcamp-full-time" : "/course/full-stack-web-development-bootcamp-part-time"}>
-                                <Button width="100px" padding="5px" onClick={() => setToggle(!toggle)} color={Colors.red} textColor={Colors.white}>GO</Button>
-                            </Link>
-                        </Row>
-                    </>
+
+                    <Row marginBottom="5px" marginRight="0" marginLeft="0" align="center">
+                        <>
+                            {locPrograms.map((item, index) => {
+                                return (
+                                    <Button width="95%" color={Colors.lightGray} textColor={Colors.gray} borderRadius=".25rem" padding="0">
+                                        <Card index="1" borders=".25rem" margin="2px 0" width="100%" padding={toggles === false && "0px"}>
+                                            <Link to={item === "16 Weeks" ? "/course/full-stack-web-development-bootcamp-part-time" : item === "9 Weeks" ? "/course/full-stack-web-development-bootcamp-full-time" : "/course/full-stack-web-development-bootcamp-part-time"}>
+                                                <Row height="25px" onClick={() => {setProgram(item), setToggles(!toggles)}} backgroundHover={Colors.lightBlue} colorHover={Colors.white} key={index} marginBottom="5px" marginTop="5px" marginRight="0" marginLeft="0" align="around">
+                                                    <Column size="12" alignSelf="center"><Paragraph fontSize="16px" color={Colors.gray} >{item}</Paragraph></Column>
+                                                </Row>
+                                            </Link>
+                                        </Card>
+                                    </Button>
+                                )
+                            })}
+                        </>
+                    </Row>
+
                     :
                     null
                 }
