16cf5e01159984db016c529e132671e83fe3ce93
plucodev <paololucanodeveloper@gmail.com>
2020-03-18 17:23:10 -0400

    fixed single post layout

--- src/templates/post.js
+++ src/templates/post.js
@@ -5,12 +5,43 @@ import {Container, Row, Column, Divider, Wrapper} from '../components/Sections'
 import {RoundImage, Colors, Check, ArrowRight} from '../components/Styling'
 import Layout from '../global/Layout'
 import twitterUser from '../utils/twitter'
+import {TwitterFollowButton} from 'react-twitter-embed';
+import {
+    FacebookShareButton,
+    TwitterShareButton,
+    TwitterIcon,
+    FacebookIcon,
+    FacebookShareCount
+} from "react-share";
+
 
 export default function Template (props) {
-    console.log("props", props);
     const {data, pageContext} = props;
     const post = props.data.markdownRemark;
+    const allowed = [`${post.frontmatter.author}`];
+    const filtered = Object.keys(twitterUser)
+        .filter(key => allowed.includes(key))
+        .reduce((obj, key) => {
+            obj = twitterUser[key];
+            return obj;
+        }, {});
 
+
+    function GetFormattedDate (date) {
+        var d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2)
+            month = '0' + month;
+        if (day.length < 2)
+            day = '0' + day;
+
+        return [year, month, day].join('-');
+    }
+    let postDate = GetFormattedDate(post.frontmatter.date)
+    console.log("date", postDate)
     return (
         <Layout type="post" seo={data.markdownRemark.frontmatter} context={pageContext}>
             <Wrapper
@@ -19,13 +50,42 @@ export default function Template (props) {
                     <Column size="8">
                         <Row height="100%" align="around">
                             <Column size="2" customRespSize respSize="2" alignSelf="center">
-                                <RoundImage border="100%" width="75px" height="75px" bsize="contain" url={post.frontmatter.avatar} />
+                                <RoundImage border="100%" width="75px" height="75px" bsize="contain" url={filtered.avatar} />
                             </Column>
-                            <Column size="10" customRespSize respSize="10" alignSelf="center">
-                                <Paragraph color={Colors.gray} align="left" fontSize="14px" lineHeight="20px">{`${post.frontmatter.date} ${post.frontmatter.author}`}</Paragraph>
-                                <Paragraph color={Colors.gray} align="left" fontSize="14px" lineHeight="20px">{`${post.fields.readingTime.text} read`}</Paragraph>
+                            <Column size="6" customRespSize respSize="10" alignSelf="center">
+                                <Row><Paragraph color={Colors.gray} align="left" fontSize="14px" lineHeight="20px">{`${postDate} - ${filtered.name}`}</Paragraph></Row>
+                                <Row><Paragraph color={Colors.gray} align="left" fontSize="14px" lineHeight="20px">{`${post.fields.readingTime.text} read`}</Paragraph></Row>
+                                <Row><TwitterFollowButton
+                                    screenName={filtered.username}
+                                /></Row>
+
+                            </Column>
+                            <Column size="" customRespSize respSize="10" alignSelf="center">
+                                <Row>
+
+                                    <TwitterShareButton
+                                        className="network__share-button"
+                                        url={`https://www.twitter.com/${filtered.username}`}
+                                        title={"share"}
+                                    >
+                                        <TwitterIcon
+                                            size={24} round={true}
+                                        />
+                                    </TwitterShareButton>
+                                    <FacebookShareButton
+                                        className="network__share-button"
+                                        url={`https://www.facebook.com/${filtered.username}`}
+                                        title={"share"}
+                                    >
+                                        <FacebookIcon
+                                            size={24} round={true}
+                                        />
+                                    </FacebookShareButton>
+
+                                </Row>
                             </Column>
                         </Row>
+                        {/* <FacebookShareCount url={"shareUrl"} /> */}
                     </Column>
                 </Row>
 
