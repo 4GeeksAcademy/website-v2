bdd05e14d27843771c0eb5dabbfd2478420bdefe
plucodev <paololucanodeveloper@gmail.com>
2020-04-17 15:46:43 -0400

    Initialized geekpal and geekforce templates

---
+++ src/templates/geekpal.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import Layout from '../global/Layout';
+import styled, {css, keyframes} from 'styled-components';
+import {Row, Column, Wrapper, Divider} from '../components/Sections'
+import {H3, Title, Separator, Paragraph} from '../components/Heading'
+import {Colors, Button} from '../components/Styling'
+import {Card} from '../components/Card'
+import {makeStyles} from '@material-ui/core/styles';
+import BaseRender from './_baseRender'
+
+const GeekPal = (props) => {
+  const {data, pageContext, yml} = props;
+
+  return (
+    <>
+      <H3>GeekPal</H3>
+    </>
+  )
+};
+export const query = graphql`
+  query GeekPalQuery($file_name: String!, $lang: String!) {
+    allPageYaml(filter: { fields: { file_name: { eq: $file_name }, lang: { eq: $lang }}}) {
+      edges{
+        node{
+            meta_info{
+                title
+                description
+                image
+                keywords
+                slug
+                
+            }
+            
+        }
+      }
+    }
+  }
+`;
+export default BaseRender(GeekPal);
