e76149a88eb120d7a4119a8fd0e976fd71a86a4c
plucodev <paololucanodeveloper@gmail.com>
2020-06-01 17:47:52 -0400

    updated events template

--- src/templates/events.js
+++ src/templates/events.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Layout from '../global/Layout';
 import styled, {css, keyframes} from 'styled-components';
 import {Row, Column, Wrapper, Divider} from '../components/Sections'
-import {H2, H3, Title, Separator, Paragraph} from '../components/Heading'
-import {Colors, Button} from '../components/Styling'
+import {H2, H3, H4, H5, Title, Separator, Paragraph} from '../components/Heading'
+import {Colors, Button, RoundImage, Address} from '../components/Styling'
 import {Card} from '../components/Card'
 import Grid from '@material-ui/core/Grid';
 import ToggleButton from '@material-ui/lab/ToggleButton';
@@ -14,29 +14,122 @@ import JobInfo from '../components/JobInfo'
 import Link from 'gatsby-link'
 
 const Events = ({data, pageContext, yml}) => {
-    console.log("jo", yml)
-    return (
-        <>
-            <Wrapper
-                style="default"
-                image="yes"
-                url={yml.banner.image}
-                border="bottom"
-                height="300px"
-                backgroundSize="cover"
-            >
-                <Divider height="100px" />
-                <Title
-                    size="5"
-                    title={yml.banner.tagline}
-                    main
-                    color={Colors.white}
-                    fontSize="46px"
-                    textAlign="center"
-                />
-            </Wrapper>
+  console.log("jo", yml)
+  const [event, setEvent] = useState([])
+  useEffect(() => {
+    const loadEvents = async () => {
+      fetch(
+        'https://assets.breatheco.de/apis/event/all',
+      )
+        .then(response => response.json())
+        .then(data => setEvent(data))
+    }
+    loadEvents();
+  }, []);
+  return (
+    <>
+      <Wrapper
+        style="default"
+        image="yes"
+        url={yml.banner.image}
+        border="bottom"
+        height="300px"
+        backgroundSize="cover"
+      >
+        <Divider height="100px" />
+        <Title
+          size="5"
+          title={yml.banner.tagline}
+          main
+          color={Colors.white}
+          fontSize="46px"
+          textAlign="center"
+        />
+      </Wrapper>
+      <Wrapper
+        style="default"
+        image="no"
+        border="top"
+        color={Colors.lightGray}
+      >
+        <Row align="left">
+          <Column size="12">
+            <H4
 
-            {/* <Wrapper
+              fs_xs="30px"
+              fs_sm="30px"
+              fs_md="30px"
+              fs_lg="30px"
+              fs_xl="30px"
+            >Featured</H4>
+          </Column>
+        </Row>
+        <Row>
+          <Separator primary />
+        </Row>
+        <Divider height="50px" />
+        <Row>
+          {event.map((i, index) => (
+            <Column size="4" key={index}>
+
+              <RoundImage
+                url={i.banner_url}
+                bsize="cover"
+                mb="10px"
+                border="1.25rem 1.25rem 0 0"
+                position="center center"
+                h_xs="230px"
+                h_sm="230px"
+                h_md="230px"
+                h_lg="230px"
+                h_xl="230px"
+              />
+              <Row marginBottom="1rem" >
+                <Column size="12">
+                  <Paragraph>{i.type}</Paragraph>
+                </Column>
+              </Row>
+              <Row marginBottom="1rem">
+                <Column size="12">
+                  <H4
+                    fs_xs="18px"
+                    fs_sm="18px"
+                    fs_md="18px"
+                    fs_lg="18px"
+                    fs_xl="18px"
+                  >{i.title}
+                  </H4>
+                </Column>
+              </Row>
+              <Row marginBottom=".2rem" >
+                <Column size="12">
+                  <Paragraph><Address width="24" color={Colors.blue} fill={Colors.blue} />{i.event_date}</Paragraph>
+                </Column>
+              </Row>
+              <Row marginBottom=".2rem" >
+                <Column size="12">
+                  <Paragraph>{i.city_slug}</Paragraph>
+                </Column>
+              </Row>
+              <Row marginBottom=".2rem" >
+                <Column size="12">
+                  <Paragraph>{i.type}</Paragraph>
+                </Column>
+              </Row>
+
+
+              {/* <Row>
+                <Column size="12">
+
+                  <div className="single-post" dangerouslySetInnerHTML={{__html: i.description}}></div>
+                </Column>
+              </Row> */}
+            </Column>
+          ))}
+        </Row>
+      </Wrapper>
+
+      {/* <Wrapper
         style="default">
         <JobInfo />
       </Wrapper>
@@ -120,8 +213,8 @@ const Events = ({data, pageContext, yml}) => {
 
       <Divider height="350px" /> */}
 
-        </>
-    )
+    </>
+  )
 };
 export const query = graphql`
   query EventsQuery($file_name: String!, $lang: String!) {
