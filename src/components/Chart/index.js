602341b5ee9bb1eef88861236740af0b4e8c4d17
plucodev <paololucanodeveloper@gmail.com>
2020-07-06 16:29:59 -0400

    Updated charts in the-academy

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PieChart from 'react-minimal-pie-chart';
 import styled, {css} from 'styled-components';
 const labelCustom = styled.div`
     font-family: 'Lato';
     font-size: 5px;
-    color: red
-    
-    
+    color: red;
 `
-
 export const Chart = (props) => {
     return (
         <>
             <PieChart
+                animate={true}
+                animationDuration={7000}
+                animationEasing="ease-out"
+                cx={50}
+                cy={50}
+                data={props.dataArray}
+                labelStyle={{
+                    fontFamily: 'Lato',
+                    fontSize: '8px'
+                }}
+
+                label={true}
+                labelPosition={70}
+                lengthAngle={360}
+                lineWidth={15}
+                tooltip={props.dataArray}
+                onClick={undefined}
+                onMouseOut={undefined}
+                onMouseOver={undefined}
+                paddingAngle={0}
+                // radius={50}
+                // rounded
+                startAngle={0}
+                viewBoxSize={[
+                    100,
+                    100
+                ]}
+            />
+        </>
+    )
+}
+
+{/* <PieChart
                 animate={true}
                 animationDuration={7000}
                 animationEasing="ease-out"
@@ -39,8 +69,4 @@ export const Chart = (props) => {
                     100,
                     100
                 ]}
-            />
-        </>
-    )
-}
-
+            /> */}
