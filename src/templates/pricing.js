f90771a6ea1906c6033f309622e1ac54012a7666
plucodev <paololucanodeveloper@gmail.com>
2020-09-15 11:40:00 -0400

    Fixed filter in select program in pricing

--- src/templates/pricing.js
+++ src/templates/pricing.js
@@ -100,7 +100,15 @@ const Pricing = (props) => {
           customRespSize
           alignResp={`center`}
         >
-          <Div alignItems={`center`}>
+          <Div
+            alignItems={`center`}
+            onMouseLeave={() => {
+
+              setTimeout(() => {
+                setPriceToggle(false);
+              }, 300)
+            }}
+          >
             <Paragraph
               fontWeight={`500`}
               fs_xs="18px"
