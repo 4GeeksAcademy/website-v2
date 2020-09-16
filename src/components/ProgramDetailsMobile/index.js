dbb5c92cc080007f23b1c91f9415f83181863884
plucodev <paololucanodeveloper@gmail.com>
2020-09-16 16:28:05 -0400

    created ProgramDetailsMobile component

---
+++ src/components/ProgramDetailsMobile/index.js
@@ -0,0 +1,133 @@
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+import {Row, Container, Column, Divider, Wrapper, Div} from '../Sections'
+import {H1, H2, H3, H4, H5, Title, Separator, Span, Paragraph} from '../Heading';
+import {Colors, TriangleDown, Laptop, Rocket, Clock} from '../Styling';
+import {Card} from '../Card';
+import Link from 'gatsby-link'
+
+
+const ProgramDetailsMobile = (props) => {
+    const [selected, setSelected] = useState({index: null, manual: false});
+    const steps = props.details.details_modules.reduce((total, current, i) => [...total, (total[i - 1] || 0) + current.step], [])
+    return (
+        <>
+            {props.details.details_modules.map((item, index) => {
+                console.log("ITEM", item)
+                return (
+                    <>
+                        <Card
+                            key={index}
+                            color={`blue`}
+                            width="100%"
+                            padding={`20px`}
+                            borders={`.5rem`}
+                            margin={`5px 0`}
+                        >
+                            <Div
+                                onClick={() => selected.index === index ? setSelected({index: null, manual: true}) : setSelected({index: index, manual: true})}
+                                // onMouseLeave={() => {
+
+                                //     setTimeout(() => {
+                                //         setSelected({index: null, manual: true})
+                                //     }, 500)
+                                // }}
+                                justifyContent={`space-between`}
+                                cursor={`pointer`}
+                            >
+                                <Div flexDirection={`column`} alignItems={`flex-start`}>
+                                    <H3
+                                        color={Colors.white}
+                                        align_sm={`left`}
+                                        align_xs={`left`}
+                                    >
+                                        {item.module_name}
+                                    </H3>
+                                    <Paragraph
+                                        color={Colors.white}
+                                        fontWeight={`500`}
+                                    >{item.duration}</Paragraph>
+                                </Div>
+                                <TriangleDown width="16" color={Colors.white} fill={Colors.white} />
+                            </Div>
+
+                        </Card>
+                        {selected.index === index &&
+                            <Card
+                                padding={`20px`}
+                            >
+                                <Div alignItems={`center`} margin={`10px 0`}>
+                                    <Laptop width="36px" fill={Colors.blue} stroke={Colors.blue} />
+                                    <H3
+                                        // fs_xs="18px"
+                                        // fs_sm="18px"
+                                        // fs_md="20px"
+                                        // fs_lg="18px"
+                                        // fs_xl="22px"
+                                        margin={`0 10px`}
+                                        fontWeight={`400`}
+                                    >
+                                        {item.module_name}
+                                    </H3>
+                                </Div>
+                                <Div >
+                                    <Paragraph
+                                        align_sm={`start`}
+                                        align_xs={`start`}
+                                    >
+                                        {item.description}
+                                    </Paragraph>
+                                </Div>
+                                <Div alignItems={`center`} margin={`10px 0`}>
+                                    <Rocket width="36px" fill={Colors.blue} stroke={Colors.blue} />
+                                    <H3
+                                        // fs_xs="18px"
+                                        // fs_sm="18px"
+                                        // fs_md="20px"
+                                        // fs_lg="18px"
+                                        // fs_xl="22px"
+                                        margin={`0 10px`}
+                                        fontWeight={`400`}
+                                    >
+                                        {`Projects`}
+                                    </H3>
+                                </Div>
+                                <Div >
+                                    <Paragraph
+                                        align_sm={`start`}
+                                        align_xs={`start`}
+                                    >
+                                        {item.projects}
+                                    </Paragraph>
+                                </Div>
+                                <Div alignItems={`center`} margin={`10px 0`}>
+                                    <Clock width="36px" fill={Colors.blue} stroke={Colors.blue} />
+                                    <H3
+                                        // fs_xs="18px"
+                                        // fs_sm="18px"
+                                        // fs_md="20px"
+                                        // fs_lg="18px"
+                                        // fs_xl="22px"
+                                        margin={`0 10px`}
+                                        fontWeight={`400`}
+                                    >
+                                        {`Duration`}
+                                    </H3>
+                                </Div>
+                                <Div >
+                                    <Paragraph
+                                        align_sm={`start`}
+                                        align_xs={`start`}
+                                    >
+                                        {item.duration}
+                                    </Paragraph>
+                                </Div>
+                            </Card>}
+                    </>
+                )
+            })}
+        </>
+    )
+}
+
+export default ProgramDetailsMobile;
