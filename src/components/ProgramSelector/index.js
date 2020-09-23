e3a738bbe695d58cacf6f17de520e5004589374e
plucodev <paololucanodeveloper@gmail.com>
2020-09-23 19:47:49 -0400

    updated programSelector. Still few issues to fix

--- src/components/ProgramSelector/index.js
+++ src/components/ProgramSelector/index.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect, useContext} from 'react';
 import styled from 'styled-components';
 import {useStaticQuery, graphql} from 'gatsby';
-import { redirectTo } from "@reach/router"
+import {redirectTo, Redirect} from "@reach/router"
 import {Row, Column} from '../Sections';
 import {Paragraph, Span} from '../Heading';
 import {Colors, Select, Option, Button, TriangleDown} from '../Styling';
@@ -38,7 +38,6 @@ const ProgramSelector = (props) => {
   const [toggle, setToggle] = useState(false)
   const [weeks, setWeeks] = useState("16");
   const classes = useStyles();
-
   let locArray = session.locations || [];
   // let locArray = data.loc.edges;
   let weekArray = ["16", "9"]
@@ -46,67 +45,94 @@ const ProgramSelector = (props) => {
   {props.week === 16 ? link = "full-time" : link = "part-time"}
   return (
     <Row align="around" marginTop={props.marginTop}>
-      <Column size="2" size_sm="3" alignSelf="center" paddingRight="0" align="right" margin="5px 0">
+      <Column size="2" size_sm="12" alignSelf="center" paddingRight="0" align="right" margin="5px 0">
         <Paragraph fontSize="20px" fs_md="14px" color={Colors.white} >It takes just </Paragraph>
       </Column>
-      <Column size="1" size_sm="3" alignSelf="center" paddingLeft="0" paddingRight="0" align="center" >
-          {toggles === false &&
-            <Button borderRadius=".25rem" onClick={() => setToggles(!toggles)} color={Colors.white} textColor={Colors.gray}>
-                {props.week}
-                <TriangleDown style={{ marginLeft: "5px"}} width="12" color={Colors.blue} fill={Colors.blue} />
-            </Button>
-          }
-          
-          {toggles == true && <Card borders=".25rem"  color="grey">
-            {weekArray.map((item, index) => {
+      <Column size="1" size_sm="12" alignSelf="center" paddingLeft="0" paddingRight="0" align="center" >
+        {toggles === false &&
+          <Button
+            style={{maxWidth: "100px", position: "relative"}}
+            onMouseLeave={() => {
+              setTimeout(() => {
+                setToggles(false);
+              }, 500)
+            }}
+            padding={`5px 10px`}
+            borderRadius=".25rem"
+            onClick={() => setToggles(!toggles)}
+            color={Colors.white}
+            textColor={Colors.gray}>
+            {weeks}
+            <TriangleDown style={{marginLeft: "5px", position: "absolute", right: "5px", top: "50%", transform: "translateY(-50%)"}} width="14" color={Colors.gray} fill={Colors.gray} />
+          </Button>
+        }
+
+        {toggles == true &&
+          <Card borders=".25rem" color="grey">
+            {Array.isArray(weekArray) && weekArray.map((item, index) => {
               return (
-                <Button 
-                width="95%" 
-                color={item === weeks ? Colors.lightGray : Colors.white} 
-                textColor={Colors.gray} 
-                borderRadius=".25rem" 
-                padding="5px"
-                onClick={() => {
-                  setWeeks(item);
-                  setToggles(!toggles);
-                  redirectTo(`/${session.language}/course/${link}`);
-                }}
+                <Button
+                  key={index}
+                  width="100%"
+                  color={item === weeks ? Colors.lightBlue : null}
+                  colorHover={Colors.lightBlue}
+                  textColor={Colors.gray}
+                  borderRadius=".25rem"
+                  padding="5px"
+                  onClick={() => {
+                    setWeeks(item);
+                    setToggles(!toggles);
+
+                    // redirectTo(`/${session.language}/course/${link}`); NOT WORKING. TEMPORARLY REPLACED WITH LINK TAG
+                  }}
                 >
-                      {item}
-                    </Button>
-                  )
-                })}
+                  <Link to={`/${session.language}/course/${link}`}>
+                    {item}
+                  </Link>
+                </Button>
+              )
+            })}
           </Card>
         }
       </Column>
-      <Column size="1" size_sm="3" alignSelf="center" paddingLeft="0" paddingRight="0" align="center" margin="5px 0"><Paragraph fontSize="20px" fs_md="14px" color={Colors.white} >weeks in</Paragraph></Column>
-      <Column size="2" size_sm="3" alignSelf="center" align="center" paddingLeft="0" paddingRight="0">
-          {toggle == false && 
-            <Button borderRadius=".25rem" onClick={() => setToggle(!toggle)} color={Colors.white} textColor={Colors.gray}>
-                {session.location && session.location.city}
-                <TriangleDown style={{ marginLeft: "5px"}} width="12" color={Colors.blue} fill={Colors.blue} />
-            </Button>
-          }
-          {toggle == true && <Card borders=".25rem" color="grey">
-            {locArray.map((item, index) => {
-                return (
-                  <Button 
-                    onClick={() => {
-                      setSession({ ...session, location: { ...item }})
-                      setToggle(!toggle)
-                    }} 
-                    colorHover={Colors.lightBlue}
-                    textColor={item.city === session.location.city ? Colors.darkGray : Colors.gray} 
-                    color={item.city === session.location.city ? Colors.lightBlue : null}
-                    borderRadius=".25rem" 
-                    padding="5px"
-                  >
-                    {item.city}
-                  </Button>
-                )
-              })}
-          </Card>
-          }
+      <Column size="1" size_sm="12" alignSelf="center" paddingLeft="0" paddingRight="0" align="center" margin="5px 0"><Paragraph fontSize="20px" fs_md="14px" color={Colors.white} >weeks in</Paragraph></Column>
+      <Column size="3" size_sm="12" alignSelf="center" align="center" paddingLeft="0" paddingRight="0">
+        {toggle == false &&
+          <Button
+            style={{maxWidth: "200px", position: "relative"}}
+
+            padding={`5px`} borderRadius=".25rem" onClick={() => setToggle(!toggle)} color={Colors.white} textColor={Colors.gray}>
+            {session.location && session.location.city}
+            <TriangleDown style={{marginLeft: "5px", position: "absolute", right: "10px", top: "50%", transform: "translateY(-50%)"}} width="14" color={Colors.gray} fill={Colors.gray} />
+          </Button>
+        }
+        {toggle == true && <Card borders=".25rem" color="grey">
+          {locArray.map((item, index) => {
+            return (
+              <Button
+                onMouseLeave={() => {
+                  setTimeout(() => {
+                    setToggle(false);
+                  }, 5000)
+                }}
+                key={index}
+                style={{maxWidth: "100%", position: "relative"}}
+                onClick={() => {
+                  setSession({...session, location: {...item}})
+                  setToggle(!toggle)
+                }}
+                colorHover={Colors.lightBlue}
+                textColor={item.city === session.location.city ? Colors.darkGray : Colors.gray}
+                color={item.city === session.location.city ? Colors.lightBlue : null}
+                borderRadius=".25rem"
+                padding="5px"
+              >
+                {item.city}
+              </Button>
+            )
+          })}
+        </Card>
+        }
       </Column>
       <Column size="2" size_sm="12" alignSelf="center" align="left" paddingLeft="0" paddingRight="0" m_sm="20px 0px 0px 0px">
         <Paragraph fontSize="20px" fs_md="14px" color={Colors.white} >to become a</Paragraph>
