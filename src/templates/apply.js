e57b26d3634f317f1c55fede8074ab668b99cafd
plucodev <paololucanodeveloper@gmail.com>
2020-10-26 11:48:38 -0400

    fixing issue #432

--- src/templates/apply.js
+++ src/templates/apply.js
@@ -117,40 +117,35 @@ const Apply = (props) => {
                     borderRadius="0 1.25rem 1.25rem 1.25rem"
                 >
                     <Column size="4" size_sm="12" color={Colors.black} br_xs="1.25rem" br_sm="1.25rem" br_md="1.25rem" h_xs="auto" h_sm="auto" h_md="auto" m_xs="35px 0" m_sm="35px 0" m_md="35px 0" alignSelf="center" height="100%"  >
-                        <Row align="center" height="100%">
+                        {/* <Row align="center" height="100%">
                             <Column size="10" height="100%">
-                                <Divider height="50px" />
-                                <Row height="60px">
-                                    <H3
-                                        fs_xs="20px"
-                                        fs_sm="18px"
-                                        fs_md="18px"
-                                        fs_lg="20px"
-                                        fs_xl="24px"
-                                        color={Colors.yellow}
-                                    >
-                                        {yml.right.heading}
-                                    </H3>
-                                </Row>
-                                <Divider height="30px" />
-
-                                {yml.right.content_section.map((item, i) => {
-                                    return (<Row key={i} height="50px">
-                                        <Paragraph
-
-                                            fs_xs="12px"
-                                            fs_sm="14px"
-                                            fs_md="10px"
-                                            fs_lg="12px"
-                                            fs_xl="14px"
-                                            lineHeight="16px"
-                                            color={Colors.lightGray}
-                                        >{item}
-                                        </Paragraph>
-                                    </Row>)
-                                })}
-                            </Column>
+                                <Divider height="50px" /> */}
+                        <Row padding={`10px`}>
+                            <H3
+                                fontSize={`22px`}
+                                color={Colors.yellow}
+                            >
+                                {yml.right.heading}
+                            </H3>
                         </Row>
+
+                        {yml.right.content_section.map((item, i) => {
+                            return (<Row key={i} margin={`10px 0`} padding={`10px`}>
+                                <Paragraph
+
+                                    fs_xs="12px"
+                                    fs_sm="14px"
+                                    fs_md="10px"
+                                    fs_lg="12px"
+                                    fs_xl="14px"
+                                    lineHeight="16px"
+                                    color={Colors.lightGray}
+                                >{item}
+                                </Paragraph>
+                            </Row>)
+                        })}
+                        {/* </Column>
+                        </Row> */}
                     </Column>
                     <Column size="8" size_sm="12" alignSelf="center" height="100%" borderRadius="0 0 0 1.25rem" color="white">
                         <Row align="center" height="100%">
