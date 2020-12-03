a379508288036ca8405e95d82fdfb060246d69da
plucodev <paololucanodeveloper@gmail.com>
2020-12-03 14:11:28 -0500

    checking test

--- src/test/test.yml.js
+++ src/test/test.yml.js
@@ -17,13 +17,14 @@ walk(`${__dirname}/../data/`, function (err, files) {
         f.indexOf('additional-redirects.yml') === -1 &&
         f.indexOf('call-to-actions.yml') === -1
     )
-
+    console.log("_files:", _files)
     let slugs = {};
     _files.forEach(_path => {
         const yml = load(_path);
-        if (yml.staff != undefined) {
-            console.log(yml.staff)
-        }
+        // if (yml.staff != undefined) {
+        //     const check = yml.staff.filter(f => f.bio.length > 10)
+        //     console.log(check)
+        // }
         if (!yml) fail("Invalid YML syntax for " + _path)
     });
     success("All YML have correct syntax")
