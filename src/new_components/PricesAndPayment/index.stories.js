18f5afade499f5aaed8a625469f9ad036d3f1a55
plucodev <paololucanodeveloper@gmail.com>
2021-02-04 17:53:19 -0500

    updated PricesAndPayments

---
+++ src/new_components/PricesAndPayment/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+
+import PricesAndPayment from '.';
+
+storiesOf('PricesAndPayment', module)
+    .add('Default', () => (
+        <PricesAndPayment lang={"us"} />
+    ));
