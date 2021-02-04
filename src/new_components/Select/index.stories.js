013b64c891bce9af1cbb51413611aaefe80f652d
plucodev <paololucanodeveloper@gmail.com>
2021-02-04 17:57:14 -0500

    updated Select

---
+++ src/new_components/Select/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+
+import ChooseProgram from '.';
+
+storiesOf('ChooseProgram', module)
+    .add('Default', () => (
+        <ChooseProgram />
+    ));
