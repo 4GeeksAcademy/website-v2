b30ce372ce307e8863883693a5a079136be87640
plucodev <paololucanodeveloper@gmail.com>
2021-02-08 09:16:01 -0500

    updated loc component

--- src/new_components/Loc/index.js
+++ src/new_components/Loc/index.js
@@ -1,23 +1,29 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {useStaticQuery, graphql} from 'gatsby';
 import {Title, H1, H2, H3, H4, Span, Paragraph, Separator} from '../Heading';
 import {Container, Row, Column, Wrapper, Divider, Grid, Div} from '../Sections'
 import {Button, Colors, RoundImage, StyledBackgroundSection} from '../Styling'
-import styled from 'styled-components';
 import Icon from "../Icon"
-import Card from '../Card';
+import {SessionContext} from '../../session.js'
 import Link from 'gatsby-link'
 
 const Loc = (props) => {
+  const {session} = useContext(SessionContext);
+  useEffect(() => {
+    const getData = async () => {
+      let resp = await fetch(`${process.env.GATSBY_BREATHECODE_HOST}/admissions/cohort/all?upcoming=true`);
+      let cohorts = await resp.json();
 
+      setData(oldData => ({
+        events: {catalog: _types, all: events, filtered: events},
+        cohorts: {catalog: oldData.cohorts.catalog, all: cohorts, filtered: cohorts}
+      }))
+    }
+    getData();
+  }, []);
   let loc = props.locations.filter(l => l.node.meta_info.unlisted != true).sort((a, b) => a.node.meta_info.position > b.node.meta_info.position ? 1 : -1)
   return (
     <Grid columns="2" gridGap="0" >
-      {/* <Row
-        github={"/location"}
-        display="flex"
-      > */}
-
       {loc != null &&
         loc.map((item, index) => {
           return (
@@ -28,10 +34,10 @@ const Loc = (props) => {
               justifyContent="between"
               height="207px"
               padding="24px"
-              background={index === 3 && Colors.lightYellow}
+              background={index === 1 && Colors.yellow}
             >
               <H3
-                align="left"
+                textAlign="left"
               >{item.node.city}
                 <Span animated color={Colors.yellow}>_</Span>
               </H3>
@@ -39,13 +45,13 @@ const Loc = (props) => {
                 display="none"
                 display_tablet="block"
               >
-                <Paragraph fontSize="15px" lineHeight="22px" color={Colors.darkGray}>
+                <Paragraph textAlign="left" fontSize="15px" lineHeight="22px" color={Colors.darkGray}>
                   Next Cohort in this location
                 </Paragraph >
-                <Paragraph fontSize="15px" lineHeight="22px" color={Colors.darkGray}>
+                <Paragraph textAlign="left" fontSize="15px" lineHeight="22px" color={Colors.darkGray}>
                   Full Stack Developer
                 </Paragraph>
-                <Paragraph fontSize="15px" lineHeight="22px" color={Colors.darkGray}>
+                <Paragraph textAlign="left" fontSize="15px" lineHeight="22px" color={Colors.darkGray}>
                   13 Jan 2021
                 </Paragraph>
               </Div>
@@ -54,10 +60,7 @@ const Loc = (props) => {
 
           )
         })
-
       }
-
-      {/* </Row> */}
     </Grid>)
 };
 
