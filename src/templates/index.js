239af4883daab5d33b16e72a8ea77ac9f5d60152
plucodev <paololucanodeveloper@gmail.com>
2021-02-08 09:11:46 -0500

    updated home

--- src/templates/index.js
+++ src/templates/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {graphql, Link, navigate} from 'gatsby';
 import {H1, H2, H3, H4, Title, Separator, Paragraph, Span} from '../new_components/Heading'
 import {Row, Column, Wrapper, Container, Grid, Div} from '../new_components/Sections'
-import {RoundImage, Colors} from '../new_components/Styling'
+import {RoundImage, Colors, StyledBackgroundSection} from '../new_components/Styling'
 import Img from 'gatsby-image'
 import News from '../new_components/News'
 import Icon from '../new_components/Icon'
@@ -44,7 +44,7 @@ const CityH1 = ({yml}) => {
     if (session.language === "es" && window.location.hash === "" && !RegExp('\/es\/inicio').test(window.location.href)) navigate("/es/inicio")
   }, [session])
 
-  return <H1 type="h1" textAlign="left" textShadow="none" fontSize="13px" color="#606060" >{city}{" "}{yml.header_data.tagline}</H1>
+  return <H1 type="h1" textAlign_md="left" textShadow="none" fontSize="13px" color="#606060" >{city}{" "}{yml.header_data.tagline}</H1>
 }
 const CityWrapper = ({yml}) => {
   const {session} = React.useContext(SessionContext);
@@ -75,84 +75,51 @@ const Home = (props) => {
 
   return (
     <>
-      <Container variant="fluid">
-        <Grid columns_tablet="2" >
-          <Div flexDirection="column" >
+      <Container variant="fluid" margin="28px 0" padding_md="0 0 0 171px" >
+        <Grid columns_md="2" >
+          <Div flexDirection="column" justifyContent_md="start" padding_md="70px 0 0 0">
             <CityH1 yml={yml} />
-            <H2
-              // padding="0 10px 0 0px"
-              // margin="16px 0 0 0"
-              fontSize="50px"
-              lineHeight="60px"
-            // textAlign="left"
-            >
-              {yml.header_data.title}
-              {/* <Span animated color={Colors.yellow}>_</Span> */}
-            </H2>
-            <Paragraph color={Colors.gray} margin="25px 0px" m_sm="20px auto" maxWidth="350px" textAlign="left" >{yml.header_data.sub_heading}</Paragraph>
+            {/* <H1 textAlign_tablet="left" margin="0 0 11px 0" color="#606060">{yml.seo_title}</H1> */}
+            <H2 textAlign_md="left" fontSize="50px" lineHeight="60px">{`${yml.header_data.title}`}</H2>
+            <Paragraph textAlign_md="left" margin="26px 0">{yml.header_data.sub_heading} </Paragraph>
+            {/* <Paragraph textAlign_tablet="left" >{yml.info_box.phone} </Paragraph>
+                    <Paragraph textAlign_tablet="left" >{yml.info_box.email} </Paragraph> */}
             <ChooseProgram
-              left="15px"
+              right="15px"
               top="40px"
+              // margin="40px 0"
+              textAlign="center"
+              textAlign_md="left"
               programs={data.allChooseProgramYaml.edges[0].node.programs}
               openLabel={data.allChooseProgramYaml.edges[0].node.close_button_text}
               closeLabel={data.allChooseProgramYaml.edges[0].node.open_button_text}
             />
             <News lang={pageContext.lang} limit={yml.news.limit} />
           </Div>
-          <Div background={Colors.gray}>test
-            {/* <Div background={Colors.blue} height="623px" width="623px" borderRadius="50%"></Div> */}
-
+          {/* <Div>
+            <Img
+              style={{height: "623px"}}
+              imgStyle={{objectFit: "contain"}}
+              alt="4Geeks Academy"
+              loading="eager"
+              fadeIn={false}
+              fluid={yml.header_data.image && yml.header_data.image.childImageSharp.fluid}
+              backgroundSize={`cover`}
+            />
+          </Div> */}
+          <Div display="none" display_md="flex" height="auto" width="100%">
+            <StyledBackgroundSection
+              height={`623px`}
+              width="100%"
+              image={yml.header_data.image && yml.header_data.image.childImageSharp.fluid}
+              bgSize={`contain`}
+            // alt={yml.header.alt}
+            />
           </Div>
         </Grid>
       </Container>
-      {/* <Row github={`/page/index.${pageContext.lang}.yml`} display={`flex`}>
-        <Column
-          size="4"
-          m_sm="0"
-          size_sm="10"
-          align_sm="center"
-          padding="100px 10px 0 10px"
-          margin="0 0 0 auto"
-        >
-          <CityH1 yml={yml} />
-
-        </Column>
-        <Column
-          size="6"
-          borderRadius="0 0 0 1.25rem"
-          height="620px"
-          customRespSize
-          disp_sm={"none"}
-          disp_xs={"none"}
-          paddingRight={`0`}
-          style={{position: "relative"}}
-        >
-
-          <Img
-            style={{height: "620px"}}
-            imgStyle={{objectFit: "contain"}}
-            alt="4Geeks Academy"
-            loading="eager"
-            fadeIn={false}
-            fluid={yml.header_data.image && yml.header_data.image.childImageSharp.fluid}
-            backgroundSize={`contain`}
-          >
-            <div style={{position: "absolute", zIndex: "1", bottom: "33px", right: "144px", transform: "translate(-50%, -50%)"}}>
-
-            <svg style={{position: "absolute", zIndex: "1", bottom: "33px", right: "144px"}} width="53" height="53" viewBox="0 0 53 53" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="26.5" cy="26.5" r="26.5" fill="#0097CD" />
-            </svg>
-            <div style={{position: "absolute", top: "50%", left: "50%"}} >test</div>
-            <imageSvg style={{position: "absolute", zIndex: "1"}} />
-            </div>
-          </Img>
-        </Column>
-      </Row> */}
-
 
       <Container variant="fluid" background="#f5f5f5" height="425px" padding="48px 0 36px 0" margin="50px 0">
-        <H2>{yml.testimonial_header.heading}</H2>
-        <Paragraph margin="25px 0 36px 0" color={Colors.blue}>{yml.testimonial_header.button_text}</Paragraph>
         <Testimonials lang={data.allTestimonialsYaml.edges} />
       </Container>
 
@@ -173,22 +140,15 @@ const Home = (props) => {
 
       {/* ABOUT 4GEEKS SECTION */}
 
-      {/* <Wrapper margin="50px 0"> */}
       <Container variant="fluid" margin="90px 0">
         <About4Geeks lang={data.allAbout4GeeksYaml.edges} />
       </Container>
-      {/* <About4Geeks lang={pageContext.lang} /> */}
-      {/* <H4 margin="50px 0">{yml.news.heading}</H4> */}
-      {/* <News lang={pageContext.lang} limit={yml.news.limit} /> */}
-      {/* </Wrapper> */}
-      {/* WHY 4GEEKS SECTION */}
 
-      <Container variant="fluid" background={Colors.verylightGray} margin="100px auto">
-        <Container variant="fixed">
-          <Credentials lang={data.allCredentialsYaml.edges} shadow={false} />
-        </Container>
-      </Container>
-
+      {/* <Container variant="fluid" background={Colors.verylightGray} margin="100px auto">
+        <Container variant="fixed"> */}
+      <Credentials lang={data.allCredentialsYaml.edges} shadow={false} />
+      {/* </Container>
+      </Container> */}
 
       <Container
         variant="fixed"
@@ -228,127 +188,12 @@ const Home = (props) => {
       </Container>
       <Container
         variant="fixed"
-        margin="100px auto"
+        margin="0 auto"
       >
         <H2 margin="0 0 15px 0" fontSize="15px" lineHeight="19px" fontWeight="900">{yml.locations.heading}</H2>
         <Paragraph margin="0 0 50px 0" >{yml.locations.sub_heading}</Paragraph>
         <Loc lang={pageContext.lang} locations={data.allLocationYaml.edges} />
       </Container>
-
-      {/* NEW COMPONENT TEST */}
-
-      {/* <WrapperCustom width="100%" style={{background: "grey"}}> */}
-
-      {/* </WrapperCustom> */}
-
-      {/* <Wrapper margin="50px 0">
-        <WhyPython lang={pageContext.lang} />
-      </Wrapper> */}
-
-
-
-      {/* GEEKS VS OTHERS SECTION */}
-
-      {/* <Wrapper margin="100px">
-        <Title
-          type="h2"
-          title={yml.geeks_vs_others.heading}
-          paragraph={yml.geeks_vs_others.sub_heading}
-          link={true}
-          linkTo={yml.geeks_vs_others.sub_heading_link}
-          paragraphColor={Colors.blue}
-          variant="primary"
-          size="10"
-        />
-        <GeeksVsOthers lang={pageContext.lang} limit={5} />
-      </Wrapper> */}
-
-      {/* ******************* */}
-      {/* JOIN 4GEEKS SECTION */}
-      {/* ******************* */}
-      {/* <Wrapper margin="100px">
-        <CityWrapper2 yml={yml} />
-        <Row github={`/page/index.${pageContext.lang}.yml`} display="flex">
-          <Column size="6" size_sm="12" >
-            <Card
-              padding="20px"
-              shadow
-              margin="10px 0px"
-            >
-              <RoundImage url="/images/geekpal.png" bsize="contain" height="40px" position="left" />
-              <Paragraph
-                color={Colors.black}
-                margin="15px 0px 15px 0px"
-                customTextAlignSmall
-                align_xs="left">
-                {yml.join_geeks.geek_data.geek_pal_heading}
-              </Paragraph>
-              <Paragraph
-                // key={index}
-                paddingRight="30px"
-                color={Colors.gray}
-                fontSize="14px"
-                lineHeight="18px"
-                customTextAlignSmall
-                align_xs="left">
-                {yml.join_geeks.geek_data.geek_pal_data.content}
-              </Paragraph>
-              <Column size="2" margin="0 0 0 auto" customRespSize align="right" paddingRight="0" respSize="2" alignSelf="flex-end">
-                <Link to={yml.join_geeks.geek_data.geek_pal_data.icon_link}>
-                  <Icon icon="arrowright" width="24px" color={Colors.blue} fill={Colors.blue} />
-                </Link>
-              </Column>
-            </Card>
-          </Column>
-          <Column size="6" size_sm="12">
-            <Card
-              padding="20px"
-              shadow
-              margin="10px 0px"
-
-            >
-              <RoundImage url="/images/geekforce.png" bsize="contain" height="40px" position="left" />
-              <Paragraph color={Colors.black} customTextAlignSmall
-                margin="15px 0px 15px 0px"
-                align_xs="left">{yml.join_geeks.geek_data.geek_force_heading}</Paragraph>
-              <Paragraph
-                key="index"
-                color={Colors.gray}
-                fontSize="14px"
-                paddingRight="30px"
-                lineHeight="18px"
-                align_xs="left">
-                {yml.join_geeks.geek_data.geek_force_data.content}
-              </Paragraph>
-              <Column size="2" margin="0 0 0 auto" paddingRight="0" align="right" alignSelf="flex-end">
-                <Link to={yml.join_geeks.geek_data.geek_force_data.icon_link}>
-                  <Icon icon="arrowright" width="24px" color={Colors.blue} fill={Colors.blue} />
-                </Link>
-              </Column>
-            </Card>
-          </Column>
-        </Row>
-      </Wrapper> */}
-
-
-
-      {/* <Wrapper
-        margin="100px">
-        <Title
-          size="10"
-          title={yml.alumni_header.heading}
-          paragraph={yml.alumni_header.sub_heading}
-          paragraphColor={Colors.darkGray}
-          maxWidth="66%"
-          margin="auto"
-          variant="primary"
-        />
-        <AlumniProjects lang={data.allAlumniProjectsYaml.edges} hasTitle showThumbs="false" limit={2} />
-      </Wrapper> */}
-
-
-
-
     </>
   )
 };
@@ -369,7 +214,7 @@ export const query = graphql`
               sub_heading
               image{
                 childImageSharp {
-                  fluid(maxWidth: 1200, quality: 100, srcSetBreakpoints: [ 200, 340, 520, 890 ]){
+                  fluid(maxWidth: 500, quality: 100, srcSetBreakpoints: [ 200, 340, 520, 890 ]){
                     ...GatsbyImageSharpFluid_withWebp
                   }
                 }
@@ -437,6 +282,7 @@ export const query = graphql`
               title
               icon
               value
+              position
             }
           }
         }
@@ -573,7 +419,7 @@ export const query = graphql`
                     }
                   } 
                 }
-                carousel_box {
+                images_box {
                   images {
                     path{
                       childImageSharp {
@@ -593,6 +439,9 @@ export const query = graphql`
           allTestimonialsYaml(filter: { fields: { lang: { eq: $lang }}}) {
             edges {
               node {
+                heading
+          button_text
+          button_link
                 testimonials {
                   student_name
                   testimonial_date
