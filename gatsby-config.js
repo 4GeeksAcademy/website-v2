f1cceb64e8ee8c96ec63297c3b3b92782b6b8549
plucodev <paololucanodeveloper@gmail.com>
2021-02-16 23:05:15 -0400

    fixed Lato font issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
         custom: {
-          families: ["Futura, Lato"],
+          families: ["Lato"],
           urls: ["/fonts/fonts.css"],
         },
       },
@@ -129,10 +129,10 @@ module.exports = {
       options: {
         env: {
           production: {
-            policy: [{ userAgent: '*' }]
+            policy: [{userAgent: '*'}]
           },
           development: {
-            policy: [{ userAgent: '*', disallow: ['/'] }]
+            policy: [{userAgent: '*', disallow: ['/']}]
           },
         }
       }
