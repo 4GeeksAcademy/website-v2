9043dd06688213eb5168fe30b17b9733747bf18e
Alejandro Sanchez <aalejo@gmail.com>
2021-03-13 00:25:11 +0000

    news and pertners can now be filtered by location

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {useStaticQuery, graphql, Link} from 'gatsby';
 import styled from "styled-components"
 import {Row, Column} from '../Sections'
+import { getStorage } from "../../actions"
 import graphic from "../../assets/images/graphic.png"
 import Img from "gatsby-image"
 
@@ -10,7 +11,8 @@ const Helper = styled.span`
   height: 100%;
   vertical-align: middle;
 `
-export default ({location, lang, limit, filter}) => {
+export default ({location, lang, limit, filter, autoTagLocation }) => {
+
   const data = useStaticQuery(graphql`
     query myQueryNews{
       allNewsYaml{
@@ -39,10 +41,19 @@ export default ({location, lang, limit, filter}) => {
 
   const languageNews = data.allNewsYaml.edges.find(({node}) => node.fields.lang === lang);
   let locationNews = typeof (languageNews) !== "object" ? [] : languageNews.node.news;
-  if (filter) locationNews = locationNews.filter(filter)
-  else if (location) locationNews = locationNews.filter(n => n.location === "all" || !location || n.location.includes(location));
+  
+  const [news, setNews] = useState(locationNews);
+  if (filter) setNews(locationNews.filter(filter))
+  else if (location) setNews(locationNews.filter(n => !n.location || !location || n.location.includes(location)));
 
-  if (limit) locationNews = locationNews.slice(0, limit)
+
+    useEffect(() => {
+        if(!location && !filter && autoTagLocation){
+            const store = getStorage("academy_session");
+            console.log("getStorage", store)
+            if(store && store.location) setNews(locationNews.filter(n => !n.location || n.location.includes(store.location.breathecode_location_slug)));
+        }
+    },[])
 
   if (locationNews.length === 0) {
     console.error(`No news to display for location `, location, locationNews)
@@ -51,7 +62,7 @@ export default ({location, lang, limit, filter}) => {
 
   return (
     <Row github="/components/news" display={`flex`}>
-      {locationNews.map((l, i) => (
+      {news.slice(0, limit).map((l, i) => (
         <Column margin="auto" style={{whiteSpace: "nowrap", height: "60px"}} key={i} size="2" size_md="3">
           <a href={l.url} target="_blank" rel="noopener noreferrer nofollow">
             <Img
