692049fe63db1e5ecf87d6a3a62c89afcd98c4c3
CSAV <calisayaariel62@gmail.com>
2021-03-15 16:54:07 -0300

    created commit messages for CRUD actions

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,10 +38,6 @@ module.exports = {
     {
       resolve:`gatsby-plugin-netlify-cms`,
       options: {
-        /**
-         * One convention is to place your Netlify CMS customization code in a
-         * `src/cms` directory.
-         */
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
