7a43f25181a2c083b7c47410118161eeb8f906da
CSAV <calisayaariel62@gmail.com>
2021-03-15 16:28:54 -0300

    added editorial-workflow created cms folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,16 @@ module.exports = {
   },
   plugins: [
     // 'gatsby-plugin-force-trailing-slashes',
-    `gatsby-plugin-netlify-cms`,
+    {
+      resolve:`gatsby-plugin-netlify-cms`,
+      options: {
+        /**
+         * One convention is to place your Netlify CMS customization code in a
+         * `src/cms` directory.
+         */
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
     'gatsby-plugin-loadable-components-ssr',
     {
       resolve: "gatsby-plugin-rollbar",
---
+++ src/cms/cms.js
@@ -0,0 +1,5 @@
+import CMS from 'netlify-cms-app'
+
+
+
+CMS.registerPreviewStyle("./md-preview-retro.css");
