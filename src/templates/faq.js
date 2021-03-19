30cf4928bd6c5e75d44b8c2f3712bc8057a23847
CSAV <calisayaariel62@gmail.com>
2021-03-19 00:36:01 -0300

    changed Row and Column to div and grid

--- src/templates/faq.js
+++ src/templates/faq.js
@@ -7,7 +7,6 @@ import {SessionContext} from '../session'
 import Link from 'gatsby-link'
 import Icon from '../new_components/Icon'
 import Card from '../new_components/Card'
-import {Container} from '../new_components/Sections'
 import {Row, Column, Divider, Div, Grid} from '../new_components/Sections'
 import {H1, H2, H3, H4, Paragraph} from '../new_components/Heading'
 
@@ -51,67 +50,65 @@ const Faq = (props) => {
                         
                         {item.questions.map((faq, index) => {
                           return (
-                            <Row display="contents">
-                                <Card
-                                    color={buttonToggle && faq.question == toggleIndex}
-                                    height="auto"
-                                    width="100%"
-                                    borders= "0"
-                                    borderBottom="1px solid"
-                                    borderColor=" #C4C4C4"
-                                    padding="20px"
-                                    onClick={() => toggleIndex === faq.question ? (setToggleIndex(undefined), setButtonToggle(!buttonToggle)) : (setToggleIndex(faq.question), setButtonToggle(true))}
-                                >
-                                    <Row key={faq.question} display="flex" height="100%">
-                                        <Div onClick={() => {setButtonToggle(!buttonToggle), setToggleIndex(toggleIndex != undefined ? undefined : faq.question)}} display="flex" width="100%" align={`center`} alignSelf="center">
-                                            <H4
-                                                type="h4"
-                                                textAlign="left"
-                                                fontSize="13px"
-                                                align={`left`}
-                                                align_sm={`left`}
-                                                color={Colors.black}
-                                                paddingRight="5%"
-                                                textTransform="uppercase"
-                                                fontWeight="700"
-                                                >{faq.question}</H4>
+                              <Card
+                                  color={buttonToggle && faq.question == toggleIndex}
+                                  height="auto"
+                                  width="100%"
+                                  borders= "0"
+                                  borderBottom="1px solid"
+                                  borderColor=" #C4C4C4"
+                                  padding="20px"
+                                  onClick={() => toggleIndex === faq.question ? (setToggleIndex(undefined), setButtonToggle(!buttonToggle)) : (setToggleIndex(faq.question), setButtonToggle(true))}
+                              >
+                                <Div key={faq.question} display="block" height="100%">
+                                    <Div onClick={() => {setButtonToggle(!buttonToggle), setToggleIndex(toggleIndex != undefined ? undefined : faq.question)}} display="flex" width="100%" align={`center`} alignSelf="center">
+                                        <H4
+                                          type="h4"
+                                          textAlign="left"
+                                          fontSize="13px"
+                                          align={`left`}
+                                          align_sm={`left`}
+                                          color={Colors.black}
+                                          paddingRight="5%"
+                                          textTransform="uppercase"
+                                          fontWeight="700"
+                                          >{faq.question}</H4>
 
-                                            {buttonToggle === false ?
-                                                toggleIndex != faq.question &&
-                                                <Icon icon="plus"
+                                        {buttonToggle === false ?
+                                            toggleIndex != faq.question &&
+                                            <Icon icon="plus"
+                                                width="24"
+                                            />
+                                            :
+                                            buttonToggle === true && toggleIndex === faq.question ?
+                                                <Icon icon="minus"
                                                     width="24"
                                                 />
                                                 :
-                                                buttonToggle === true && toggleIndex === faq.question ?
-                                                    <Icon icon="minus"
-                                                        width="24"
-                                                    />
-                                                    :
-                                                    <Icon icon="plus"
-                                                        width="24"
-                                                    />
-                                            }
-                                        </Div>
+                                                <Icon icon="plus"
+                                                    width="24"
+                                                />
+                                        }
+                                    </Div>
 
-                                        <Div size="12" size_sm="12" alignSelf="center">
-                                            {buttonToggle === true && toggleIndex === faq.question &&
-                                                <Paragraph
-                                                    
-                                                    textAlign="left"
-                                                    letterSpacing="0.05em"
-                                                    lineHeight="22px"
-                                                    fontWeight="normal"
-                                                    dangerouslySetInnerHTML={{__html: faq.answer}}
-                                                    margin={`20px 0 0 0`}
-                                                    align_sm="left"
-                                                    fontFamily="Lato, sans-serif">
-                                                </Paragraph>
-                                            }
-                                        </Div>
+                                    <Div size="12" size_sm="12" alignSelf="center">
+                                        {buttonToggle === true && toggleIndex === faq.question &&
+                                            <Paragraph
+                                                
+                                                textAlign="left"
+                                                letterSpacing="0.05em"
+                                                lineHeight="22px"
+                                                fontWeight="normal"
+                                                dangerouslySetInnerHTML={{__html: faq.answer}}
+                                                margin={`20px 0 0 0`}
+                                                align_sm="left"
+                                                fontFamily="Lato, sans-serif">
+                                            </Paragraph>
+                                        }
+                                    </Div>
 
-                                    </Row>
-                                </Card>
-                            </Row >
+                                </Div>
+                            </Card>
                           )}
                         )}
                         </>
