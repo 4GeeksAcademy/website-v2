d9c3cbbd067f247369916780b0c4df3251861088
plucodev <paololucanodeveloper@gmail.com>
2021-05-25 10:08:09 -0400

    Fixed issue in Mobile Navbar

--- src/new_components/NavbarMobile/index.js
+++ src/new_components/NavbarMobile/index.js
@@ -181,7 +181,7 @@ export const MegaMenu = ({status, setStatus, menu, languageButton, currentURL, s
                     {status.toggle && status.itemIndex == null && <Menu>
                         {menu && menu.map((item, index) => {
                             return (
-                                <MenuItem onClick={() => setStatus({...status, itemIndex: index})}>
+                                <MenuItem key={index} onClick={() => setStatus({...status, itemIndex: index})}>
                                     {index != menu.length - 1 ?
                                         <>
                                             <H3 textAlign="left" width="fit-content" margin="0 5px 0 0" fontSize="13px" lineHeight="16px" fontWeight="400">{item.name}</H3>
@@ -200,7 +200,7 @@ export const MegaMenu = ({status, setStatus, menu, languageButton, currentURL, s
                             <>
                                 {Array.isArray(menu[status.itemIndex].sub_menu.links) && menu[status.itemIndex].sub_menu.links.map((m, i) => {
                                     return (
-                                        <>
+                                        <React.Fragment key={i}>
                                             <H3 textAlign="left" fontSize="15px" lineHeight="22px" fontWeight="900" margin="15px 0 5px 0">{m.title}</H3>
                                             {m.sub_links != undefined && Array.isArray(m.sub_links) && m.sub_links.map((m, i) => {
                                                 return (
@@ -218,25 +218,12 @@ export const MegaMenu = ({status, setStatus, menu, languageButton, currentURL, s
                                                         return (
                                                             <Link to={m.link} key={i}>
                                                                 <H3 textAlign="left" width="fit-content" fontSize="15px" lineHeight="22px" fontWeight="400" margin="10px 5px 0 0">{m.text}</H3>
-                                                                {/* <Button
-                                                                    variant="outline"
-                                                                    color="black"
-                                                                    font='"Lato", sans-serif'
-                                                                    width="fit-content"
-                                                                    margin="20px 10px 0 0"
-                                                                    pointer
-                                                                    textColor={Colors.black}
-                                                                    fontSize={"13px"}
-                                                                    borderRadius="3px" padding="10px"
-                                                                >
-                                                                    {m.text}
-                                                                </Button> */}
                                                             </Link>
                                                         )
                                                     })}
                                                 </Div>
                                             }
-                                        </>
+                                        </React.Fragment>
                                     )
                                 })}
                             </>
