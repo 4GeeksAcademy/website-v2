4753dab964dc6dee07602567ff2d5cacaf841fb5
ArielCalisaya <calisayaariel62@gmail.com>
2021-06-08 22:22:04 -0400

    removed unused console.logs and const

--- src/templates/calendar.js
+++ src/templates/calendar.js
@@ -73,10 +73,8 @@ const Calendar = (props) => {
   useEffect(() => {
     const getData = async () => {
       let resp = await fetch(`${process.env.GATSBY_BREATHECODE_HOST}/admissions/cohort/all?upcoming=true`);
-      // let resp = await fetch(`https://breathecode.herokuapp.com/v1/admissions/cohort/all?upcoming=true`);
       let cohorts = await resp.json();
       let resp2 = await fetch(`${process.env.GATSBY_BREATHECODE_HOST}/events/all`);
-      // let resp2 = await fetch(`https://breathecode.herokuapp.com/v1/events/all`);
       let events = await resp2.json();
       let _types = []
       for (let i = 0; i < events.length; i++) {
