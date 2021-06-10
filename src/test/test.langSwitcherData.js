df1323db2ce0708c814b8fcd54d19c17e09ee2ce
ArielCalisaya <calisayaariel62@gmail.com>
2021-06-10 02:54:14 -0400

    created langSwitcherData generator json for pathsDictionary (actions.worker.js)

--- src/test/test.langSwitcherData.js
+++ src/test/test.langSwitcherData.js
@@ -1,37 +1,51 @@
-const path = require("path");
 const fs = require("fs");
 const { walk, loadYML, empty, fail, success } = require("./_utils");
 
 const onCreateLangSwitcherData = () => {
 
   let arraySlug= []
+  let langES = []
+  let langUS = []
+  let SWITCHER_LANG_ES_US = []
+  let SWITCHER_LANG_US_ES = []
   walk(`${__dirname}/../data/page`, (err, files) => {
     if (err) fail("Error reding the Page files: ", err);
-    
       for (let i = 0; i < files.length; i++) {
         const _path = files[i];
         const doc = loadYML(_path);
-        const fileName = _path.replace(/^.*[\\\/]/, '').split('.').slice(0, -1).join('.').toLowerCase();
-        const getName= fileName.split(".")[0]
-        const getLang= fileName.split(".")[1]
-        // TODO: TENEMOS LO QUE QUERIAMOS dividimos name y lang ahora a usar logica
-        console.log("FILENAME::SPLIT:::",getName)
-        console.log("FILENAME::SPLIT:::",getLang)
-
-        // arraySlug.push(fileName)
-
-
-        // console.log("PATH:::", fileName)
-        //TODO: buscar forma de enviar.es al inicio como /es/ o /us/
-        // console.log("ARRAY::::::::::::::", arraySlug[i] === arraySlug[i-1])
+        const lang = doc.lang
+        const slug = doc.yaml.meta_info.slug
+        arraySlug.push([`/${lang}/${slug}`])
+      }
+      for(let l=0; l < arraySlug.length; l++){
+        if (l % 2 == 0) {
+          langES.push(arraySlug[l])
+        }
+        if (l % 2 == 1) {
+          langUS.push(arraySlug[l])
+        }
       }
 
+      for(let z=0; z < langES.length; z++){
+        SWITCHER_LANG_ES_US.push(langES[z].concat(langUS[z]))
+        SWITCHER_LANG_US_ES.push(langUS[z].concat(langES[z]))
+      }
+      console.log("Total pages in spanish:", langES.length)
+      console.log("Total pages in english:", langUS.length)
+
+      Object.fromEntries(SWITCHER_LANG_ES_US)
+      Object.fromEntries(SWITCHER_LANG_US_ES)
+      
+      SWITCHER_LANG_ES_US.push(...SWITCHER_LANG_US_ES)
+      let parsedToKeyValue = Object.fromEntries(SWITCHER_LANG_ES_US)
+
+      // console.log("SWITCHER::", parsedToKeyValue)
       //TODO esto esta listo ok!!!
-      // console.log("ARRAY::::::::::::::", arraySlug)
-      // fs.writeFile(`${__dirname}/../utils/langSwitcherData.json`, JSON.stringify(arraySlug), (err) => {
-      //   if (err) return console.log(err);
-      //   console.log('langSwitcherData > langSwitcherData.json');
-      // });
+
+      fs.writeFile(`${__dirname}/../utils/langSwitcherData.json`, JSON.stringify(parsedToKeyValue), (err) => {
+        if (err) return console.log(err);
+        console.log(`\nlangSwitcherData => /src/utils/langSwitcherData.json\n`);
+      });
   });
 
 };
