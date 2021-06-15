3600e9b130ead531b046a7a15b514c3b7adf98f5
ArielCalisaya <calisayaariel62@gmail.com>
2021-06-15 17:50:33 -0400

    Home: added svg bubbles on mobile

--- src/templates/index.js
+++ src/templates/index.js
@@ -79,6 +79,19 @@ const CityH1 = ({yml}) => {
 //   />
 // }
 
+const SVGBubblesLeft = () => 
+  <svg style={{top: "62px", left: "0", position: "absolute", zIndex: -1}} width="26" height="362" viewBox="0 0 26 362" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="-2.5" cy="333.5" r="28.5" fill="#FFB718" fill-opacity="0.2"/>
+    <circle cx="-10.5" cy="26.5" r="26.5" fill="#0097CD"/>
+  </svg>
+
+const SVGBubblesRight = () =>
+  <svg style={{top: "10px", right: "0", position: "absolute", zIndex: -1}} width="50" height="160" viewBox="0 0 50 160" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12.5" cy="73.5" r="12.5" fill="#CD0000"/>
+    <circle cx="89" cy="80" r="80" fill="#FFB718" fill-opacity="0.2"/>
+  </svg>
+
+
 
 const Home = (props) => {
 
@@ -100,7 +113,17 @@ const Home = (props) => {
 
   return (
     <>
-      <GridContainerWithImage padding_tablet="0" columns_tablet="2" margin="72px 0 71px 0" margin_tablet="72px 0 108px 0" >
+      <Div 
+        display="flex" 
+        position="absolute"
+        width="100%"
+        zIndex="0"
+        display_tablet="none"
+      >
+        <SVGBubblesLeft />
+        <SVGBubblesRight />
+      </Div>
+      <GridContainerWithImage padding="65px 5px 0 5px" padding_tablet="0" columns_tablet="2" margin="72px 0 72px 0" margin_tablet="72px 0 108px 0" >
         <Div flexDirection="column" justifyContent_tablet="evenly" alignItems="center" alignItems_tablet="start">
           <Div flexDirection="column" alignItems="center" alignItems_tablet="start">
             <CityH1 yml={yml} />
