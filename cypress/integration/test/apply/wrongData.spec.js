d4d010c227f98830496f6b26bf0f22f69ebbe60b
ArielCalisaya <calisayaariel62@gmail.com>
2021-07-27 16:52:58 -0400

    test secret env

--- cypress/integration/test/apply/wrongData.spec.js
+++ cypress/integration/test/apply/wrongData.spec.js
@@ -1,38 +1,34 @@
-context("Test Apply page with wrong data", () => {
+context("Test Contact page with wrong data", () => {
 
-  it("Call the form and fill with wrong values", () => {
-    cy.visit("/us/apply").wait(4000);
-    // It gets data in fixtures folder to fill form
-    cy.fixture("/apply/names.json").then((data) => {
-      const { firstName, lastName } = data.robot;
-
-      cy.get("[data-cy=first_name]").type(firstName);
-    });    
+  it('Visit the Contact page with path "/us/contact"', () => {
+    cy.visit("/us/contact").wait(500);
   });
 
-  it("Fill the input fields with wrong values", () => {
-    cy.fixture("/apply/form_values/wrong.json").each((wrong) => {
+  it("Call the form and fill with wrong values", () => {
+    // It gets data in fixtures folder to fill form
+    cy.fixture("/contact/wrong.json").each((wrong) => {
+      const { firstName, lastName, email, comment } = wrong;
+
+      cy.get("[data-cy=first_name]")
+        .clear()
+        .type(firstName)
+
+      cy.get("[data-cy=last_name]")
+        .clear()
+        .type(lastName)
+
       cy.get("[data-cy=email]")
         .clear()
-        .type(wrong.email)
+        .type(email)
         .should("have.css", "background-color", "rgb(255, 205, 201)"); // reject input color
       cy.get("span").contains("Please specify a valid email");
-  
-      cy.get("[data-cy=phone]")
-        .clear()
-        .type(wrong.phone)
-        .should("have.css", "background-color", "rgb(255, 205, 201)");
-      cy.get("span").contains("Please specify a valid phone number");
-    });
-  })
 
-  it("Select Program level 2", () => {
-    cy.get("[data-cy=dropdown_program_selector]").click().wait(500); // Gets Drowpdown of Courses
-    cy.get("#react-select-2-option-1").click(); // Selects Level with option 1
-  })
+      cy.get("[data-cy=comment]").clear().type(comment);
+    });
+  });
+
   it("Shouldn't submit the form", () => {
-    cy.get('Button[type="submit"]').contains("APPLY").click().wait(500);
-    cy.get('[data-cy=alertText]') // Alert after submit
-    // cy.get(".Form__Alert-gbvxik") // Alert after submit
-  })
+    cy.get('Button[type="submit"]').contains("Send").click();
+    cy.get("[data-action=alert-message]") // Alert after submit
+  });
 });
