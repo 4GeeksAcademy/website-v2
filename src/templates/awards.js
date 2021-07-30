711245b1506e1a13c969506fd8d0a7eb76e953ce
ArielCalisaya <calisayaariel62@gmail.com>
2021-07-29 22:54:21 -0400

    updated awards heading page

--- src/templates/awards.js
+++ src/templates/awards.js
@@ -37,6 +37,33 @@ const SVGBubblesRight = () =>
         <rect x="169" y="212" width="119" height="11" rx="5.5" fill="black"/>
     </svg>
 
+const SVGImage = () =>
+    <svg width="100%" height="284" viewBox="0 0 419 284" fill="none" xmlns="https:://www.w3.org/2000/svg">
+        <rect x="419" y="284" width="419" height="11" rx="5.49998" transform="rotate(-180 419 284)" fill="#A4A4A4" />
+        <rect x="133" y="257" width="133" height="11" rx="5.49999" transform="rotate(-180 133 257)" fill="#A4A4A4" />
+        <rect x="276" y="257" width="133" height="11" rx="5.49999" transform="rotate(-180 276 257)" fill="#A4A4A4" />
+        <rect x="133" y="229" width="133" height="11" rx="5.49999" transform="rotate(-180 133 229)" fill="#A4A4A4" />
+        <rect x="276" y="229" width="133" height="11" rx="5.49999" transform="rotate(-180 276 229)" fill="#A4A4A4" />
+        <rect x="276" y="199" width="133" height="11" rx="5.49999" transform="rotate(-180 276 199)" fill="#A4A4A4" />
+        <rect x="419" y="257" width="133" height="11" rx="5.49999" transform="rotate(-180 419 257)" fill="#A4A4A4" />
+        <circle cx="209.5" cy="81.5" r="58.5" fill="#FFB718" />
+        <circle cx="238.5" cy="23.5" r="23.5" fill="#0097CD" />
+        <circle cx="336.5" cy="81.5" r="8.5" fill="#C7F3FD" />
+        <circle cx="368.5" cy="136.5" r="8.5" fill="#C7F3FD" />
+        <circle cx="336.5" cy="116.5" r="8.5" fill="black" />
+        <circle cx="368.5" cy="171.5" r="8.5" fill="black" />
+        <circle cx="336.5" cy="171.5" r="8.5" fill="#C7F3FD" />
+        <circle cx="368.5" cy="214.5" r="8.5" fill="#C7F3FD" />
+        <circle cx="368.5" cy="100.5" r="8.5" transform="rotate(90 368.5 100.5)" fill="#C7F3FD" />
+        <circle cx="336.5" cy="214.5" r="8.5" fill="#C7F3FD" />
+        <circle cx="14" cy="122" r="14" fill="#FFB718" />
+        <circle cx="95.5" cy="81.5" r="8.5" transform="rotate(90 95.5 81.5)" fill="#C7F3FD" />
+        <rect x="74" y="119" width="77" height="11" rx="5.5" transform="rotate(90 74 119)" fill="black" />
+        <circle cx="53.5" cy="67.5" r="13.5" fill="#CD0000" />
+        <path d="M206.902 129.5C208.057 127.5 210.943 127.5 212.098 129.5L232.45 164.75C233.604 166.75 232.161 169.25 229.852 169.25H189.148C186.839 169.25 185.396 166.75 186.55 164.75L206.902 129.5Z" fill="#CD0000" />
+        <path d="M184.5 147.5L156.5 153.5V132.5L134 121.5L142 104.5L124 85.5L138.5 68.5L124 54.5L148.5 41" stroke="#FFB718" strokeWidth="4" strokeLinecap="round" />
+        <path d="M236 148.027L263.769 154V133.093L286.083 122.142L278.149 105.218L296 86.3022L281.62 69.3778L296 55.44L271.702 42" stroke="#FFB718" strokeWidth="4" strokeLinecap="round" />
+    </svg>
 
 
 const Awards = ({data, pageContext, yml}) => {
@@ -52,7 +79,7 @@ const Awards = ({data, pageContext, yml}) => {
                 <SVGBubblesLeft />
                 <SVGBubblesRight />
             </Div>
-            <GridContainer padding_tablet="80px 0" padding="40px 17px 80px 17px" background="rgba(199, 243, 253, 0.5)" margin="60px 0 0 0">
+            <GridContainer padding_tablet="80px 0" padding="60px 17px 80px 17px" background="rgba(199, 243, 253, 0.5)" margin="60px 0 0 0">
                 <Div 
                     // gridColumn_tablet="1 / 6"
                     flexDirection="column" justifyContent_tablet="start" padding_tablet="60px 0">
