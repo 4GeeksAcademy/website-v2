d82eaf9a20fc06400eff836b3ef569d8ec25c567
LuxSalgado <Luis14salgado@gmail.com>
2021-09-27 19:45:16 -0400

    Added useState and useEffect to templates/course-engineering.js

--- src/templates/course-engineering.js
+++ src/templates/course-engineering.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,  {useState, useEffect} from 'react';
 import Link from 'gatsby-link'
 import {GridContainer, Header, Div} from '../new_components/Sections'
 import {Button, Colors} from '../new_components/Styling'
