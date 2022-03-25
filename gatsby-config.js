4d5ed426e5f2b2cd16d715ca36def86b943f8c4c
gustavomm19 <gustavomm199@gmail.com>
2022-03-25 20:07:46 +0000

    testing exclude preview

--- gatsby-config.js
+++ gatsby-config.js
@@ -158,7 +158,7 @@ module.exports = {
             allSitePage(
               filter: {
                 context: {visibility: {nin: ["hidden", "unlisted"]}}
-                nodes: { path: { regex: "^((?!/preview).)*$" } }
+                nodes: { path: { regex: "/^((?!\/preview).)*$/" } }
               }
             ) {
                 nodes {
