ac0dd18221cf7bf058768174b7dee024a8a9c46e
gustavomm19 <gustavomm199@gmail.com>
2022-09-30 16:59:30 +0000

    filter jobs by visibility

--- src/components/JobInfo/index.js
+++ src/components/JobInfo/index.js
@@ -21,7 +21,7 @@ import Fragment from "../Fragment";
 const JobInfo = () => {
   const data = useStaticQuery(graphql`
     query myNewJobsQuery {
-      allJobYaml(filter: { meta_info: { open: { ne: false } } }) {
+      allJobYaml(filter: { meta_info: { visibility: { nin: ["hidden", "unlisted"] } } }) {
         edges {
           node {
             banner_heading
