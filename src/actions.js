71ff6412e126c919afa3628cd619018de7c30a6b
gustavomm19 <gustavomm199@gmail.com>
2022-10-11 21:59:44 +0000

    move console logs

--- src/actions.js
+++ src/actions.js
@@ -2,8 +2,6 @@ import { save_form } from "./utils/leads";
 
 const getToken = async (action) => {
   let token = null;
-  console.log('process.env.GOOGLE_CAPTCHA_KEY');
-  console.log(process.env.GOOGLE_CAPTCHA_KEY);
   try {
     token = await grecaptcha.execute(process.env.GOOGLE_CAPTCHA_KEY, {
       action,
@@ -198,6 +196,8 @@ export const apply = async (data, session) => {
   const automation = body.automation || "strong";
 
   const action = "submit";
+  console.log("process.env.GOOGLE_CAPTCHA_KEY");
+  console.log(process.env.GOOGLE_CAPTCHA_KEY);
   let token = await getToken(action);
 
   if (!session || !session.utm || !session.utm.utm_test)
