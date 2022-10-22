5f09e4516369eade1b76c57ad6f583766a333526
Alejandro Sanchez <aalejo@gmail.com>
2022-10-22 01:51:24 +0000

    fixed blog post como pagar

--- src/templates/landing_a.js
+++ src/templates/landing_a.js
@@ -266,9 +266,8 @@ const Landing = (props) => {
                 // textShadow="0px 0px 4px black"
                 textAlign="left"
                 color={Colors.white}
-              >
-                {yml.features.text}
-              </Paragraph>
+                dangerouslySetInnerHTML={{ __html: yml.features.text }}
+              />
             )}
             {yml.short_badges && (
               <Div
