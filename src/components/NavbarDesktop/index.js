63499f962b535e7dd929884892b5681405ef4218
gustavomm19 <gustavomm199@gmail.com>
2022-11-18 05:20:03 +0000

    navbbar fixed with css

--- src/components/NavbarDesktop/index.js
+++ src/components/NavbarDesktop/index.js
@@ -31,10 +31,10 @@ const Triangle = styled.div`
   position: absolute;
   width: 0;
   height: 0;
-  border-left: 40px solid transparent;
-  border-right: 40px solid transparent;
+  border-left: 50px solid transparent;
+  border-right: 50px solid transparent;
   border-bottom: 60px solid white;
-  top: -55px;
+  top: -40px;
   left: ${(props) => props.left};
 `;
 const MegaMenuPositions = [
@@ -48,7 +48,7 @@ const MegaMenuPositions = [
   {
     top: "",
     left: "50%",
-    leftTriangle: "36%",
+    leftTriangle: "31%",
     transform: "translateX(-480px)",
     width: "815px",
     margin: "10px",
@@ -56,7 +56,7 @@ const MegaMenuPositions = [
   {
     top: "",
     left: "50%",
-    leftTriangle: "32%",
+    leftTriangle: "23%",
     transform: "translateX(-200px)",
     width: "377px",
     margin: "0",
@@ -64,7 +64,7 @@ const MegaMenuPositions = [
   {
     top: "",
     left: "50%",
-    leftTriangle: "32%",
+    leftTriangle: "26%",
     transform: "translateX(-150px)",
     width: "610px",
     margin: "10px",
@@ -111,8 +111,8 @@ const MenuItem = styled.li`
   margin: 0 0.5rem;
   text-align: center;
   font-family: lato, sans-serif;
-  &:hover{
-    .megamenu-container{
+  &:hover {
+    .megamenu-container {
       display: block;
     }
   }
@@ -253,14 +253,13 @@ export const Navbar = ({
                   {index !== menu.length - 1 && <Icon icon="arrowdown" />}
                   <MegaMenu
                     key={item.name}
-                    index={index}
                     status={{
                       ...status,
                       toggle: !status.toggle,
                       itemIndex: index,
                     }}
                     setStatus={setStatus}
-                    menu={menu} 
+                    menu={menu}
                   />
                 </MenuItem>
               );
@@ -374,219 +373,210 @@ const CampusMenu = ({ status, setStatus, menu }) => {
   );
 };
 
-export const MegaMenu = ({ status, setStatus, menu, index }) => {
+export const MegaMenu = ({ status, setStatus, menu }) => {
   return (
     <>
-      {
-        status.itemIndex !== null &&
-        status.itemIndex !== menu.length - 1 && (
-          <MegaMenuContainer
-            className="megamenu-container"
-            onMouseLeave={() => {
-              setStatus({ ...status, hovered: false });
-              setTimeout(() => {
-                setStatus((_status) => ({
-                  ..._status,
-                  toggle: _status.hovered,
-                }));
-              }, 300);
-            }}
-            display={index === 0 ? "block" : "none"}
-            // display="none"
-            maxHeight="500px"
-            background="white"
-            transform={MegaMenuPositions[status.itemIndex].transform}
-            width={MegaMenuPositions[status.itemIndex].width}
-            padding_tablet="30px 30px 45px 30px"
-            position="absolute"
-            top="100px"
-            left={
-              status.itemIndex == 0
-                ? "0"
-                : MegaMenuPositions[status.itemIndex].left
-            }
-            zIndex_tablet="1"
-            borderRadius="3px"
-            minWidth_tablet={status.itemIndex == 0 ? "100%" : "432px"}
-            maxWidth_tablet="100%"
-            minHeight_tablet="347px"
-            boxShadow_tablet="0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"
-            boxShadow="0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"
+      {status.itemIndex !== null && status.itemIndex !== menu.length - 1 && (
+        <MegaMenuContainer
+          className="megamenu-container"
+          onMouseLeave={() => {
+            setStatus({ ...status, hovered: false });
+            setTimeout(() => {
+              setStatus((_status) => ({
+                ..._status,
+                toggle: _status.hovered,
+              }));
+            }, 300);
+          }}
+          display="none"
+          maxHeight="500px"
+          background="white"
+          transform={MegaMenuPositions[status.itemIndex].transform}
+          width={MegaMenuPositions[status.itemIndex].width}
+          padding_tablet="30px 30px 45px 30px"
+          position="absolute"
+          top="80px"
+          left={
+            status.itemIndex == 0
+              ? "0"
+              : MegaMenuPositions[status.itemIndex].left
+          }
+          zIndex_tablet="1"
+          borderRadius="3px"
+          minWidth_tablet={status.itemIndex == 0 ? "100%" : "432px"}
+          maxWidth_tablet="100%"
+          minHeight_tablet="347px"
+          boxShadow_tablet="0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"
+          boxShadow="0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"
+        >
+          <Triangle left={MegaMenuPositions[status.itemIndex].leftTriangle} />
+          <Grid
+            gridTemplateColumns_tablet="repeat(12, 1fr)"
+            gridTemplateRows="2"
+            width="100%"
           >
-            <Triangle left={MegaMenuPositions[status.itemIndex].leftTriangle} />
-            <Grid
-              gridTemplateColumns_tablet="repeat(12, 1fr)"
-              gridTemplateRows="2"
-              width="100%"
+            <Div
+              borderBottom_tablet="1px solid #EBEBEB"
+              gridArea_tablet="1/1/1/13"
+              padding="0 0 27px 0"
+              margin={MegaMenuPositions[status.itemIndex].margin}
             >
-              <Div
-                borderBottom_tablet="1px solid #EBEBEB"
-                gridArea_tablet="1/1/1/13"
-                padding="0 0 27px 0"
-                margin={MegaMenuPositions[status.itemIndex].margin}
-              >
-                {menu[status.itemIndex].sub_menu.icon && (
-                  <Div margin="0 15px 0 0">
-                    <Icon
-                      icon={menu[status.itemIndex].sub_menu.icon}
-                      width="43px"
-                      height="34px"
-                    />
-                  </Div>
+              {menu[status.itemIndex].sub_menu.icon && (
+                <Div margin="0 15px 0 0">
+                  <Icon
+                    icon={menu[status.itemIndex].sub_menu.icon}
+                    width="43px"
+                    height="34px"
+                  />
+                </Div>
+              )}
+              <Div flexDirection="column">
+                {status.itemIndex != null && (
+                  <Link
+                    to={
+                      menu[status.itemIndex].sub_menu.link &&
+                      menu[status.itemIndex].sub_menu.link
+                    }
+                  >
+                    <Div alignItems="baseline" margin="5px 0 ">
+                      <H3
+                        textAlign="left"
+                        width="fit-content"
+                        fontSize="15px"
+                        lineHeight="20px"
+                        fontWeight="400"
+                        margin="0 5px 0 0"
+                      >
+                        {menu[status.itemIndex].sub_menu.title}
+                      </H3>
+                      {menu[status.itemIndex].sub_menu.link && (
+                        <Icon
+                          icon="arrow-right"
+                          color="#A4A4A4"
+                          width="8px"
+                          height="8px"
+                        />
+                      )}
+                    </Div>
+                  </Link>
                 )}
-                <Div flexDirection="column">
-                  {status.itemIndex != null && (
-                    <Link
-                      to={
-                        menu[status.itemIndex].sub_menu.link &&
-                        menu[status.itemIndex].sub_menu.link
-                      }
-                    >
-                      <Div alignItems="baseline" margin="5px 0 ">
+                {/* <H3 textAlign="left" fontSize="15px" lineHeight="22px" fontWeight="900" margin="0 0 5px 0">{status.itemIndex != null && menu[status.itemIndex].sub_menu.title}</H3> */}
+                {menu[status.itemIndex].sub_menu.paragraph
+                  .split("\n")
+                  .map((d, i) => (
+                    <Paragraph textAlign="left" color={Colors.darkGray} key={i}>
+                      {d}
+                    </Paragraph>
+                  ))}
+              </Div>
+            </Div>
+
+            <Div gridArea_tablet="2/1/2/13">
+              <Grid
+                gridTemplateColumns_tablet={`repeat(${
+                  menu[status.itemIndex].sub_menu.links.length
+                }, 1fr)`}
+                width="100%"
+              >
+                {menu[status.itemIndex].name !== "Campus" &&
+                  Array.isArray(menu[status.itemIndex].sub_menu.links) &&
+                  menu[status.itemIndex].sub_menu.links.map((m, i) => {
+                    return (
+                      <Div flexDirection="column" key={i}>
+                        {m.icon && (
+                          <Icon icon={m.icon} width="100px" height="73px" />
+                        )}
+                        {m.level && (
+                          <H4
+                            textAlign="left"
+                            margin="19px 0 5px 0"
+                            fontSize="15px"
+                            fontWeight="400"
+                            lineHeight="22px"
+                          >
+                            {m.level}
+                          </H4>
+                        )}
                         <H3
                           textAlign="left"
-                          width="fit-content"
                           fontSize="15px"
-                          lineHeight="20px"
-                          fontWeight="400"
-                          margin="0 5px 0 0"
+                          lineHeight="22px"
+                          fontWeight="900"
+                          margin="0 0 5px 0"
                         >
-                          {menu[status.itemIndex].sub_menu.title}
+                          {m.title != "-" ? m.title : <span>&nbsp;</span>}
                         </H3>
-                        {menu[status.itemIndex].sub_menu.link && (
-                          <Icon
-                            icon="arrow-right"
-                            color="#A4A4A4"
-                            width="8px"
-                            height="8px"
-                          />
+                        {m.paragraph && (
+                          <Paragraph textAlign="left">{m.paragraph}</Paragraph>
                         )}
-                      </Div>
-                    </Link>
-                  )}
-                  {/* <H3 textAlign="left" fontSize="15px" lineHeight="22px" fontWeight="900" margin="0 0 5px 0">{status.itemIndex != null && menu[status.itemIndex].sub_menu.title}</H3> */}
-                  {menu[status.itemIndex].sub_menu.paragraph
-                    .split("\n")
-                    .map((d, i) => (
-                      <Paragraph
-                        textAlign="left"
-                        color={Colors.darkGray}
-                        key={i}
-                      >
-                        {d}
-                      </Paragraph>
-                    ))}
-                </Div>
-              </Div>
-
-              <Div gridArea_tablet="2/1/2/13">
-                <Grid
-                  gridTemplateColumns_tablet={`repeat(${
-                    menu[status.itemIndex].sub_menu.links.length
-                  }, 1fr)`}
-                  width="100%"
-                >
-                  {menu[status.itemIndex].name !== "Campus" &&
-                    Array.isArray(menu[status.itemIndex].sub_menu.links) &&
-                    menu[status.itemIndex].sub_menu.links.map((m, i) => {
-                      return (
-                        <Div flexDirection="column" key={i}>
-                          {m.icon && (
-                            <Icon icon={m.icon} width="100px" height="73px" />
-                          )}
-                          {m.level && (
-                            <H4
-                              textAlign="left"
-                              margin="19px 0 5px 0"
-                              fontSize="15px"
-                              fontWeight="400"
-                              lineHeight="22px"
-                            >
-                              {m.level}
-                            </H4>
-                          )}
-                          <H3
-                            textAlign="left"
-                            fontSize="15px"
-                            lineHeight="22px"
-                            fontWeight="900"
-                            margin="0 0 5px 0"
-                          >
-                            {m.title != "-" ? m.title : <span>&nbsp;</span>}
-                          </H3>
-                          {m.paragraph && (
-                            <Paragraph textAlign="left">
-                              {m.paragraph}
-                            </Paragraph>
-                          )}
-                          {m.buttons != undefined && (
-                            <Div>
-                              {Array.isArray(m.buttons) &&
-                                m.buttons.map((m, i) => {
-                                  return (
-                                    <Link to={m.link} key={i}>
-                                      <Button
-                                        variant="outline"
-                                        color="black"
-                                        font='"Lato", sans-serif'
-                                        width="fit-content"
-                                        margin="20px 10px 0 0"
-                                        pointer
-                                        textColor={Colors.black}
-                                        fontSize={"13px"}
-                                        borderRadius="3px"
-                                        padding="10px"
-                                      >
-                                        {m.text}
-                                      </Button>
-                                    </Link>
-                                  );
-                                })}
-                            </Div>
-                          )}
-                          {m.sub_links != undefined &&
-                            Array.isArray(m.sub_links) &&
-                            m.sub_links.map((m, i) => {
-                              return (
-                                <Link to={m.link_to} key={i}>
-                                  <Div
-                                    margin="2px 0"
-                                    padding="10px 0 10px 18px"
-                                    backgroundHover={`#E6F5FB`}
-                                    borderRadius="3px"
-                                    alignItems="baseline"
-                                  >
-                                    <H3
-                                      textAlign="left"
+                        {m.buttons != undefined && (
+                          <Div>
+                            {Array.isArray(m.buttons) &&
+                              m.buttons.map((m, i) => {
+                                return (
+                                  <Link to={m.link} key={i}>
+                                    <Button
+                                      variant="outline"
+                                      color="black"
+                                      font='"Lato", sans-serif'
                                       width="fit-content"
-                                      fontSize="15px"
-                                      lineHeight="20px"
-                                      fontWeight="400"
-                                      margin="0 5px 0 0"
+                                      margin="20px 10px 0 0"
+                                      pointer
+                                      textColor={Colors.black}
+                                      fontSize={"13px"}
+                                      borderRadius="3px"
+                                      padding="10px"
                                     >
-                                      {m.title}
-                                    </H3>
-                                    <Icon
-                                      icon="arrow-right"
-                                      color="#A4A4A4"
-                                      width="8px"
-                                      height="8px"
-                                    />
-                                  </Div>
-                                </Link>
-                              );
-                            })}
-                        </Div>
-                      );
-                    })}
-                </Grid>
-              </Div>
-            </Grid>
-            {menu[status.itemIndex].name === "Campus" && (
-              <CampusMenu status={status} setStatus={setStatus} menu={menu} />
-            )}
-          </MegaMenuContainer>
-        )}
+                                      {m.text}
+                                    </Button>
+                                  </Link>
+                                );
+                              })}
+                          </Div>
+                        )}
+                        {m.sub_links != undefined &&
+                          Array.isArray(m.sub_links) &&
+                          m.sub_links.map((m, i) => {
+                            return (
+                              <Link to={m.link_to} key={i}>
+                                <Div
+                                  margin="2px 0"
+                                  padding="10px 0 10px 18px"
+                                  backgroundHover={`#E6F5FB`}
+                                  borderRadius="3px"
+                                  alignItems="baseline"
+                                >
+                                  <H3
+                                    textAlign="left"
+                                    width="fit-content"
+                                    fontSize="15px"
+                                    lineHeight="20px"
+                                    fontWeight="400"
+                                    margin="0 5px 0 0"
+                                  >
+                                    {m.title}
+                                  </H3>
+                                  <Icon
+                                    icon="arrow-right"
+                                    color="#A4A4A4"
+                                    width="8px"
+                                    height="8px"
+                                  />
+                                </Div>
+                              </Link>
+                            );
+                          })}
+                      </Div>
+                    );
+                  })}
+              </Grid>
+            </Div>
+          </Grid>
+          {menu[status.itemIndex].name === "Campus" && (
+            <CampusMenu status={status} setStatus={setStatus} menu={menu} />
+          )}
+        </MegaMenuContainer>
+      )}
     </>
   );
 };
