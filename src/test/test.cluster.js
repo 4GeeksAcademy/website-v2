4fb40b99467ddec3565ec46717c892bb1dc807ac
gustavomm19 <gustavomm199@gmail.com>
2022-12-21 18:47:28 +0000

    making tests for clusters

---
+++ src/test/test.cluster.js
@@ -0,0 +1,101 @@
+var colors = require("colors");
+const {
+  walk,
+  loadYML,
+  empty,
+  fail,
+  warn,
+  success,
+  validateObjectProperties,
+  getEntityTypeFromPath,
+} = require("./_utils");
+
+const metas = [
+  { key: "slug", type: "string", mandatory: true },
+  { key: "title", type: "string", mandatory: true },
+  { key: "description", type: "string", length: 160 },
+  { key: "image", type: "string" },
+  { key: "keywords", type: "string" },
+  { key: "redirects", type: "array" },
+];
+
+const mustHaveAcademies = (val) => {
+  if (!Array.isArray(val))
+    throw Error(
+      `academies property should be an array and its a ${typeof val}`
+    );
+  else if (val.length == 0)
+    throw Error(
+      "Pricing plan has no academies assigned, comment the plan instead of leaving the academies array empty"
+    );
+};
+const validateEmptyPlan = (val) => {
+  if (val.slug == undefined) throw Error("Pricing plan missing slug");
+
+  if (!Array.isArray(val.bullets))
+    throw Error(
+      `bullets property should exist and be array and its ${typeof val.bullets}`
+    );
+  else if (val.bullets.length == 0)
+    throw Error(
+      "Pricing plan bullets are missing, please add at least one bullet"
+    );
+};
+
+const validateEmpty = (val, breathecrumb) => {
+  if (!val || val === "") throw Error("Missing value" + breathecrumb);
+};
+
+let duplicateDescriptions = {};
+walk(`${__dirname}/../data/cluster/`, function (err, files) {
+  if (err) fail("Error reding the YML files: ", err);
+  const _files = files.filter(
+    (f) => f.indexOf(".yml") > 1 || f.indexOf(".yaml") > 1
+  );
+
+  let langs = {};
+  let slugs = {};
+  _files.forEach((_path) => {
+    const doc = loadYML(_path);
+    langs[doc.lang] = true;
+    if (!slugs[doc.name]) slugs[doc.name] = {};
+    if (!slugs[doc.name][doc.lang]) slugs[doc.name][doc.lang] = {};
+
+    const yml = doc && doc.yaml;
+    if (!yml) fail("Invalid YML syntax for " + _path);
+    else {
+      // look for duplicated slugs
+      try {
+        const validateSlug = (val, breadcrumbPath) => {
+          // if(slugs[doc.name][doc.lang][val]) throw Error(`Plan slug ${val} already found for ${slugs[doc.name][doc.lang][val]} (please remove duplicate)`)
+          // else slugs[doc.name][doc.lang][val] = breadcrumbPath;
+          slugs[doc.name][doc.lang][val] = breadcrumbPath;
+        };
+
+        validateObjectProperties(doc.yaml, {
+          "header.title": validateEmpty,
+        });
+      } catch (error) {
+        fail(`${error.message} in ${error.path} for file: \n ${_path}`);
+      }
+    }
+  });
+
+  // for (let course in slugs) {
+  //   for (let lang in langs) {
+  //     if (!slugs[course][lang])
+  //       fail(
+  //         `Missing pricing plans for ${course} in language: ${lang}, maybe you want to create a ./src/data/plans/${course}.${lang}.yml file`
+  //       );
+  //     for (let planName in slugs[course][lang]) {
+  //       for (let otherLang in langs) {
+  //         if (!slugs[course][otherLang][planName])
+  //           fail(
+  //             `Pricing plan ${planName} is missing a "${otherLang}" translation for the course ${course}.\nTo fix this, open the ./src/data/plans/${course}.${otherLang}.yml file and add a plan with slug "${planName}"`
+  //           );
+  //       }
+  //     }
+  //   }
+  // }
+  success("All the culters are OK!");
+});
