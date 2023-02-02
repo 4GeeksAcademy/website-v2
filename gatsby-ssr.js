d2ba921daade16a764911e782b3b57f799758d63
gustavomm19 <gustavomm199@gmail.com>
2023-02-02 21:38:37 +0000

    revert ssr change

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,14 +1,7 @@
-const React = require("react");
-const Layout = require("./src/components/layout");
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ */
 
-// Adds a class name to the body element
-exports.onRenderBody = ({ setBodyAttributes }, pluginOptions) => {
-  setBodyAttributes({
-    className: "my-body-class",
-  });
-};
-
-// Wraps every page in a component
-exports.wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>;
-};
+// You can delete this file if you're not using it
