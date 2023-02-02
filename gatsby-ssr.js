950c366d55a2898945fc328187b1ad3cd7703257
gustavomm19 <gustavomm199@gmail.com>
2023-02-02 21:33:21 +0000

    add gatsby-ssr.js content

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,14 +1,14 @@
-const React = require("react")
-const Layout = require("./src/components/layout")
+const React = require("react");
+const Layout = require("./src/components/layout");
 
 // Adds a class name to the body element
 exports.onRenderBody = ({ setBodyAttributes }, pluginOptions) => {
   setBodyAttributes({
     className: "my-body-class",
-  })
-}
+  });
+};
 
 // Wraps every page in a component
 exports.wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>
-}
+  return <Layout {...props}>{element}</Layout>;
+};
