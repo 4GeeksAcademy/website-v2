8a0b278230e5344f2eb071c10afd5bf6193d5b5a
gustavomm19 <gustavomm199@gmail.com>
2023-03-01 00:42:45 +0000

    fix message

--- src/templates/_baseLandingLayout.js
+++ src/templates/_baseLandingLayout.js
@@ -47,7 +47,7 @@ const BaseRender =
         }));
       if (filteredPrograms.length == 0) {
         throw new Error(
-          "There are not programs to show on this landing page, make sure to include them on meta_info.utm_course array and inside the components.choose_program component YML with the visibility 'visible' or 'unlisted'"
+          "There are not programs to show on this landing page, make sure to include them on meta_info.utm_course array and inside the course folder with the visibility 'visible' or 'unlisted'"
         );
       }
     } else {
