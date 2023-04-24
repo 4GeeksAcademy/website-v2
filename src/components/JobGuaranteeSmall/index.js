bed042d3320126ddfd34005c467c59c7fdb93a5d
Gustavo Márquez <47191295+gustavomm19@users.noreply.github.com>
2023-04-24 23:18:44 +0000

    doing component

---
+++ src/components/JobGuaranteeSmall/index.js
@@ -0,0 +1,18 @@
+import React, { useRef } from "react";
+import { H2, H3, H4, Paragraph } from "../Heading";
+import Slider from "react-slick";
+import { Div, HR } from "../Sections";
+import { StyledBackgroundSection } from "../Styling";
+import { Button, Colors } from "../Styling";
+import Icon from "../Icon";
+import { smartRedirecting } from "../../utils/utils.js";
+
+const JobGuaranteeSmall = ({ content }) => {
+  return (
+    <Div>
+      {content.title}
+    </Div>
+  );
+}
+
+export default JobGuaranteeSmall;
