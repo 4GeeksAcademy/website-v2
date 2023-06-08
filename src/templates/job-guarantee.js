9e88431431f96b1015356dd5e6c7e263a07d78ea
Gustavo <gustavomm199@gmail.com>
2023-06-07 21:12:45 -0400

    added new component

--- src/templates/job-guarantee.js
+++ src/templates/job-guarantee.js
@@ -11,6 +11,7 @@ import Modal from "../components/Modal";
 import LeadForm from "../components/LeadForm";
 import { Circle } from "../components/BackgroundDrawing";
 import Testimonials from "../components/Testimonials";
+import Icon from "../components/Icon";
 import OurPartners from "../components/OurPartners";
 import ScholarshipProjects from "../components/ScholarshipProjects";
 import ScholarshipSuccessCases from "../components/ScholarshipSuccessCases";
@@ -309,8 +310,8 @@ const JobGuarantee = ({ data, pageContext, yml }) => {
       <Div
         id="two_column_left"
         flexDirection="column"
-        padding="50px 0 50px 0"
-        padding_tablet="50px 6%"
+        padding="0 0 50px 0"
+        padding_tablet="50px 6% 0 6%"
         margin="0"
       >
         <TwoColumn
@@ -334,12 +335,65 @@ const JobGuarantee = ({ data, pageContext, yml }) => {
         margin="0"
       >
         <Div
-          padding="20px 10px"
+          padding="50px 10px"
           padding_tablet="30px"
           margin="0"
           background={Colors.lightBlue}
+          display="block"
         >
-          <H2 textAlign="left">{yml.we_trust_section.title}</H2>
+          <Div display="block">
+            <H2 textAlign="left" margin="0 0 15px 0">{yml.we_trust_section.title}</H2>
+            <Paragraph
+              textAlign="left"
+              color={Colors.darkGray}
+              opacity="1"
+              fontSize="18px"
+              dangerouslySetInnerHTML={{ __html: yml.we_trust_section.text }}
+            />
+          </Div>
+          <Div
+            margin="15px 0 0 0"
+            justifyContent_tablet="between"
+            gap="15px"
+            flexDirection="column"
+            flexDirection_tablet="row"
+          >
+            {yml.we_trust_section.boxes.map((box) => (
+              <Div
+                key={box.title}
+                background="#FFF"
+                border="3px solid #000"
+                width="100%"
+                width_md="320px"
+                height_md="320px"
+                width_tablet="200px"
+                height_tablet="200px"
+                boxShadow="6px 6px 0px 0px rgba(0,0,0,1)"
+                boxShadow_tablet="9px 8px 0px 0px rgba(0,0,0,1)"
+                flexDirection_tablet="column"
+                justifyContent_tablet="center"
+                padding="15px"
+                alignItems="center"
+                alignItems_tablet="normal"
+              >
+                <Icon icon={box.icon} width="89px" height="89px" color={null}/>
+                <Div margin="0 0 0 15px" margin_tablet="20px 0 0 0" display="flex" flexDirection="column" display_tablet="block">
+                  <H3
+                    textAlign="left"
+                    fontWeight="900"
+                    fontSize="30px"
+                    fontSize_tablet="50px"
+                    margin="0 0 20px 0"
+                  >
+                    {box.title}
+                  </H3>
+                  <Paragraph textAlign="left" color="#000" opacity="1" fontSize="18px">
+                    {box.text}
+                  </Paragraph>
+                </Div>
+              </Div>
+            ))}
+          </Div>
         </Div>
       </Div>
       <ScholarshipSuccessCases
