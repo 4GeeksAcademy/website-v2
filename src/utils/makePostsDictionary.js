ba534c4fcc5184a915c8baefac77821aafcf652f
gustavomm19 <gustavomm199@gmail.com>
2023-07-27 19:11:18 +0000

    add default translation to makePostsDictionary

--- src/utils/makePostsDictionary.js
+++ src/utils/makePostsDictionary.js
@@ -4,6 +4,11 @@ const API = require("../utils/api");
 const file = require("./dictionaries/pages.json");
 
 const toKeyValue = (array) => Object.fromEntries(array);
+const defaultCluster = {
+  us: "trends-and-tech",
+  es: "tendencias-y-tecnologia",
+  en: "trends-and-tech",
+};
 
 const onCreateBlogLangSwitcher = async () => {
   const All_dictionary = [];
@@ -21,19 +26,19 @@ const onCreateBlogLangSwitcher = async () => {
   for (let i = 0; i < posts.length; i++) {
     const post = posts[i];
     const lang = post.lang;
-    const cluster = post.clusters.length > 0 ? post.clusters[0] : "";
+    const cluster = post.clusters.length > 0 ? post.clusters[0] : defaultCluster[lang];
     const slug = post.slug;
 
     let translation;
     if (lang === "es") {
       if ("us" in post.translations)
         translation = posts.find(
-          (obj) => obj.slug === post.translations.us && obj.clusters.length > 0
+          (obj) => obj.slug === post.translations.us
         );
       blog_ES_US.push([
         `/${lang}/${cluster}/${slug}`,
         translation
-          ? `/us/${translation.clusters[0]}/${translation.slug}`
+          ? `/us/${translation.clusters.length > 0 ? translation.clusters[0] : defaultCluster[translation.lang]}/${translation.slug}`
           : "/us/blog",
       ]);
     } else {
@@ -44,7 +49,7 @@ const onCreateBlogLangSwitcher = async () => {
       blog_US_ES.push([
         `/${lang}/${cluster}/${slug}`,
         translation
-          ? `/es/${translation.clusters[0]}/${translation.slug}`
+          ? `/es/${translation.clusters.length > 0 ? translation.clusters[0] : defaultCluster[translation.lang]}/${translation.slug}`
           : "/es/blog-en-espanol",
       ]);
     }
