7c440aeba9a30283ecd0b498759a563c3a009eb1
gustavomm19 <gustavomm199@gmail.com>
2023-08-01 00:26:11 +0000

    Add rediection and path tests to website

---
+++ src/test/test.redirect.js
@@ -0,0 +1,29 @@
+const {
+  loadYML,
+  fail,
+} = require("./_utils");
+const fs = require("fs");
+const pages = require("../utils/dictionaries/pages.json");
+
+async function listDir(dir) {
+  try {
+    return await fs.promises.readdir(`${__dirname}${dir}`);
+  } catch (err) {
+    console.error("Error occurred while reading directory!", err);
+  }
+}
+
+const folders = ['course', 'location', 'cluster', 'page', 'location', 'landing', 'downloadable'];
+folders.forEach(async (folder) => {
+  const files = await listDir(`/../data/${folder}`);
+  files.map((_path) => {
+    const file = loadYML(`src/data/${folder}/${_path}`);
+    if(file.yaml.meta_info.redirects.some((redirect) => Object.keys(pages.yml[0]).includes(redirect)))
+      fail(
+        `${`\nProblem found in: src/data/${folder}/${_path}`.red}\n\n${
+          `A path found in the redirects array is already being use by another page`.red
+        }\n`
+      );
+    
+  });
+});
