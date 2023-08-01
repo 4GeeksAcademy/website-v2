bb1438a309ec1122c3f62e61ca395e8e250cd556
gustavomm19 <gustavomm199@gmail.com>
2023-08-01 00:27:32 +0000

    Add rediection and path tests to website

--- src/test/test.redirect.js
+++ src/test/test.redirect.js
@@ -1,7 +1,4 @@
-const {
-  loadYML,
-  fail,
-} = require("./_utils");
+const { loadYML, fail } = require("./_utils");
 const fs = require("fs");
 const pages = require("../utils/dictionaries/pages.json");
 
@@ -13,17 +10,29 @@ async function listDir(dir) {
   }
 }
 
-const folders = ['course', 'location', 'cluster', 'page', 'location', 'landing', 'downloadable'];
+const folders = [
+  "course",
+  "location",
+  "cluster",
+  "page",
+  "location",
+  "landing",
+  "downloadable",
+];
 folders.forEach(async (folder) => {
   const files = await listDir(`/../data/${folder}`);
   files.map((_path) => {
     const file = loadYML(`src/data/${folder}/${_path}`);
-    if(file.yaml.meta_info.redirects.some((redirect) => Object.keys(pages.yml[0]).includes(redirect)))
+    if (
+      file.yaml.meta_info.redirects.some((redirect) =>
+        Object.keys(pages.yml[0]).includes(redirect)
+      )
+    )
       fail(
         `${`\nProblem found in: src/data/${folder}/${_path}`.red}\n\n${
-          `A path found in the redirects array is already being use by another page`.red
+          `A path found in the redirects array is already being use by another page`
+            .red
         }\n`
       );
-    
   });
 });
