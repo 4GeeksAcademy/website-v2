950250c881c0c5f3728a1b70fee939229567db44
Alejandro Sanchez <aalejo@gmail.com>
2023-09-07 21:23:59 +0000

    fixed issue with chicago lat,long and added new validation to avoid two locations with the same lat,long

--- src/test/test.location.js
+++ src/test/test.location.js
@@ -74,7 +74,7 @@ walk(`${__dirname}/../data/location`, async (err, files) => {
       f.indexOf("call-to-actions.yml") === -1
   );
 
-  let allCoordinates = {}
+  let allCoordinates = {};
   let slugs = [];
 
   for (let i = 0; i < _files.length; i++) {
@@ -160,16 +160,29 @@ walk(`${__dirname}/../data/location`, async (err, files) => {
         }
       });
 
-      
-      if(allCoordinates[location.latitude] != undefined && allCoordinates[location.latitude] != location.breathecode_location_slug)
-        fail(`❌ ERROR: Location ${location.breathecode_location_slug} has same latitude as ${allCoordinates[location.latitude]}\n`);
-      else allCoordinates[location.latitude] = location.breathecode_location_slug;
-      
-      if(allCoordinates[location.longitude] != undefined && allCoordinates[location.longitude] != location.breathecode_location_slug)
-      fail(`❌ ERROR: Location ${location.breathecode_location_slug} has same longitude as ${allCoordinates[location.longitude]}\n`);
-      else allCoordinates[location.longitude] = location.breathecode_location_slug;
+      if (
+        allCoordinates[location.latitude] != undefined &&
+        allCoordinates[location.latitude] != location.breathecode_location_slug
+      )
+        fail(
+          `❌ ERROR: Location ${
+            location.breathecode_location_slug
+          } has same latitude as ${allCoordinates[location.latitude]}\n`
+        );
+      else
+        allCoordinates[location.latitude] = location.breathecode_location_slug;
 
-      console.log("allCoordinates", allCoordinates)
+      if (
+        allCoordinates[location.longitude] != undefined &&
+        allCoordinates[location.longitude] != location.breathecode_location_slug
+      )
+        fail(
+          `❌ ERROR: Location ${
+            location.breathecode_location_slug
+          } has same longitude as ${allCoordinates[location.longitude]}\n`
+        );
+      else
+        allCoordinates[location.longitude] = location.breathecode_location_slug;
 
       locations_fields.forEach((field) => {
         if (!meta_keys.includes(field["key"]) && field["mandatory"] === true)
