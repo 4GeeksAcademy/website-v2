a7c2843546f34041fab25ba91cb20714ad33b7ec
Kelvin Ruiz <k3azzz@gmail.com>
2023-09-14 01:37:44 -0800

    ajustes responsive, tamano de letras

--- src/components/CardsCarousel/index.js
+++ src/components/CardsCarousel/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
 import PropTypes from "prop-types";
-import { Button, Colors, Img} from "../Styling";
+import { Button, Colors, Img } from "../Styling";
 import { Grid, Div } from "../Sections";
 import { H4, H3, H2, Paragraph } from "../Heading";
 import Icon from "../Icon";
@@ -48,51 +48,89 @@ const CardsCarousel = ({
         }
     }
 `);
-    return (
-        <Grid
-            gridTemplateColumns_tablet="2fr repeat(14, 1fr) 2fr"
-            flexDirection_tablet="column"
-            maxWidth_tablet="1366px"
-        >
-            {title &&
-                <H2 margin="0 0 10px 0"
-                    fontWeight="700"
-                >
-                    {title.text}
-                </H2>
-            }
 
-            {sub_title && /<\/?[a-z0-9]+>/g.test(sub_title.text) ? (
+    return (
+        <Div
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            maxWidth_tablet="1366px"
+            margin="auto"
+        >
+            <Div
+                display="flex"
+                flexDirection="column"
+            >
+                {title &&
+                    <H2
+                        margin="0 0 10px 0"
+                        fontWeight="700"
+                        fontSize={title.font_size[0]}
+                        lineHeight="36px"
+                    >
+                        {title.text}
+                    </H2>
+                }
+
+                {sub_title && /<\/?[a-z0-9]+>/g.test(sub_title.text) ? (
+                    <Paragraph
+                        margin="10px 0"
+                        fontSize={sub_title.font_size[0]}
+                        dangerouslySetInnerHTML={{ __html: sub_title.text }}
+                    />
+                ) : sub_title &&
                 <Paragraph
                     margin="10px 0"
-                    dangerouslySetInnerHTML={{ __html: sub_title.text }}
-                />
-            ) : sub_title &&
-            <Paragraph
-                margin="10px 0"
-            >
-                {sub_title.text}
-            </Paragraph>
-            }
-
+                    fontSize={sub_title.font_size[0]}
+                >
+                    {sub_title.text}
+                </Paragraph>
+                }
+            </Div>
             <Div
-                width="100%"
+                width_xs="80%"
+                width_md="85%"
                 className="badge-slider hideOverflowX__"
                 flexDirection="row"
-                m_sm="0px 0px 100px 0"
-                justifyContent="between"
+                marginTablet="97px 0"
+                justifyContent_tablet="flex-start"
+                justifyContent_md={cards.length > 3 ? "flex-start" : "center"}
             >
                 {cards.map((card, index) => (
                     <Div
+                        key={index}
+                        flexDirection="column"
+                        width="266px"
+                        height="auto"
+                        border="2px solid black"
+                        margin="0 12px"
                     >
                         <Img
                             src={card.image.src}
+                            width="262px"
+                            height="266px"
                         />
-                        <H3>
+                        <H3
+                            textTransform="uppercase"
+                            fontSize={card.heading.font_size}
+                            fontWeight="900"
+                            lineHeight="19px"
+                        >
                             {card.heading.text}
                         </H3>
-                        <Button>
+                        <Button
+                            textTransform="uppercase"
+                            fontSize="16px"
+                            fontWeight="700"
+                            lineHeight="19px"
+                        >
                             {card.button.text}
+                            <Icon
+                                icon="arrow-right"
+                                width="15px"
+                                height="15px"
+                                padding="0 0 0 5px"
+                            />
                         </Button>
                     </Div>
                 )
@@ -134,7 +172,7 @@ const CardsCarousel = ({
                     {button.text}
                 </Button>
             }
-        </Grid>
+        </Div>
     )
 }
 export default CardsCarousel;
