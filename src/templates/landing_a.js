f78b685fa1ca2eaa8f79896e31debc26c2df78f1
Gustavo L <95352419+G-code7@users.noreply.github.com>
2023-12-08 14:22:14 +0000

    downloadable fix it

--- src/templates/landing_a.js
+++ src/templates/landing_a.js
@@ -81,27 +81,25 @@ const Landing = (props) => {
     };
   }, []);
 
-  const landingLocation =
-    session &&
-    session.locations?.find(
-      (l) => l.breathecode_location_slug === yml.meta_info.utm_location[0]
-    );
-
   // const landingLocation =
   //   session &&
-  //   (yml.meta_info.utm_location.length > 1
-  //     ? session.locations?.find(
-  //         (l) =>
-  //           l.breathecode_location_slug ===
-  //           yml.meta_info.utm_location?.find(
-  //             (l) => l === session.location?.breathecode_location_slug
-  //           )
-  //       )
-  //     : session.locations?.find(
-  //         (l) => l.breathecode_location_slug === yml.meta_info.utm_location[0]
-  //       ));
+  //   session.locations?.find(
+  //     (l) => l.breathecode_location_slug === yml.meta_info.utm_location[0]
+  //   );
 
-  console.log(landingLocation, session, yml.meta_info.utm_location);
+  const landingLocation =
+    session &&
+    (yml.meta_info.utm_location?.length > 1
+      ? session.locations?.find(
+          (l) =>
+            l.breathecode_location_slug ===
+            yml.meta_info.utm_location?.find(
+              (l) => l === session.location?.breathecode_location_slug
+            )
+        )
+      : session.locations?.find(
+          (l) => l.breathecode_location_slug === yml.meta_info.utm_location[0]
+        ));
 
   return (
     <>
