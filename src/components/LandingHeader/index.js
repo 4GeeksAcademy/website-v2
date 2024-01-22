80dc66de3fee8ce8b8b70c42dec055f0517ed8b5
Gustavo L <gust.20220@gmail.com>
2024-01-22 16:31:54 +0000

    img in horizontal

--- src/components/LandingHeader/index.js
+++ src/components/LandingHeader/index.js
@@ -82,14 +82,14 @@ const LandingHeader = (props) => {
             {yml.header_data.partner_logo_url && (
               <>
                 <Div
-                  width="242px"
+                  width="100%"
                   flexDirection_tablet="column"
                   height="auto"
                   padding="0 0 25px 0"
                 >
                   <GatsbyImage
                     loading="eager"
-                    imgStyle={{ objectFit: "contain" }}
+                    imgStyle={{ "objectFit": "contain" }}
                     image={getImage(
                       yml.header_data.partner_logo_url.childImageSharp
                         .gatsbyImageData
