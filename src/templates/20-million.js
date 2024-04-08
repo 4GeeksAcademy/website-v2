4490689d2de95e40109dd6aced1c6ec01a5e1541
khrisefzm <khrisefzm@gmail.com>
2024-04-08 16:12:32 -0500

    first section done

--- src/templates/20-million.js
+++ src/templates/20-million.js
@@ -3,9 +3,14 @@ import { graphql } from "gatsby";
 import { Link } from "gatsby";
 import { isCustomBarActive } from "../actions";
 import BaseRender from "./_baseLayout";
-import { Div, HR, GridContainer } from "../components/Sections";
-import { H2, Paragraph } from "../components/Heading";
-import { Button, Colors } from "../components/Styling";
+import {
+  Div,
+  HR,
+  GridContainer,
+  GridContainerWithImage,
+} from "../components/Sections";
+import { H1, H2, Paragraph } from "../components/Heading";
+import { Button, Colors, StyledBackgroundSection } from "../components/Styling";
 import { beHiringPartner } from "../actions";
 import { SessionContext } from "../session";
 import LeadForm from "../components/LeadForm";
@@ -44,273 +49,138 @@ const TwentyMillion = ({ data, pageContext, yml }) => {
 
   const ymlTwoColumn = yml?.two_column_left;
 
+  const isCustomBarActive = true;
+  // session &&
+  // session.location &&
+  // session.location.custom_bar &&
+  // session.location.custom_bar.active;
+
   return (
     <>
       <Div
-        margin={
-          isCustomBarActive(session)
-            ? "138px auto 30px auto"
-            : "72px auto 30px auto"
-        }
-        margin_md={
-          isCustomBarActive(session)
-            ? "120px auto 30px auto"
-            : "72px auto 30px auto"
-        }
-        padding="90px 20px 42px 20px"
-        padding_tablet="72px 130px 72px 130px"
+        margin={isCustomBarActive ? "0 auto 0 auto" : "72px auto 0 auto"}
+        margin_md={isCustomBarActive ? "120px auto 0 auto" : "72px auto 0 auto"}
+        margin_sm="0 auto 0 auto"
+        padding="20px 20px 0 20px"
+        padding_tablet="0 80px"
         position="relative"
-        background={Colors.veryLightBlue2}
         display="block"
       >
-        <Circle
-          color="yellow"
-          width="17px"
-          height="17px"
-          top="87px"
-          left="74px"
-          zIndex="1"
-          display="none"
-          display_tablet="inline"
-        />
-        <Circle
-          color="black"
-          width="17px"
-          height="17px"
-          top="116px"
-          left="74px"
-          zIndex="1"
-          display="none"
-          display_tablet="inline"
-        />
-        <Circle
-          color="white"
-          width="17px"
-          height="17px"
-          top="172px"
-          left="74px"
-          zIndex="-1"
-          display="none"
-          display_tablet="inline"
-        />
-        <Circle
-          color="blue"
-          width="17px"
-          height="17px"
-          top="216px"
-          left="74px"
-          zIndex="-1"
-          display="none"
-          display_tablet="inline"
-        />
-        <Circle
-          color="white"
-          width="17px"
-          height="17px"
-          top="298px"
-          left="74px"
-          zIndex="-1"
-          display="none"
-          display_tablet="inline"
-        />
-        <Circle
-          color="black"
-          width="17px"
-          height="17px"
-          top="116px"
-          left="106px"
-          zIndex="-1"
-          display="none"
-          display_tablet="inline"
-        />
-        <Circle
-          color="white"
-          width="17px"
-          height="17px"
-          top="145px"
-          left="106px"
-          zIndex="-1"
-          display="none"
-          display_tablet="inline"
-        />
-        <Circle
-          color="white"
-          width="17px"
-          height="17px"
-          top="182px"
-          left="106px"
-          zIndex="1"
-          display="none"
-          display_tablet="inline"
-        />
-        <Circle
-          color="yellow"
-          width="17px"
-          height="17px"
-          top="246px"
-          left="106px"
-          zIndex="1"
-          display="none"
-          display_tablet="inline"
-        />
-        <Circle
-          color="blue"
-          width="30px"
-          height="30px"
-          top="120px"
-          right="83px"
-          zIndex="1"
-          display="none"
-          display_tablet="inline"
-        />
-        <Circle
-          color="white"
-          width="17px"
-          height="17px"
-          top="170px"
-          right="50px"
-          zIndex="1"
-          display="none"
-          display_tablet="inline"
-        />
-        <Circle
-          color="black"
-          width="17px"
-          height="17px"
-          top="170px"
-          right="89px"
-          zIndex="1"
-          display="none"
-          display_tablet="inline"
-        />
-        <Circle
-          color="white"
-          width="17px"
-          height="17px"
-          top="170px"
-          right="128px"
-          zIndex="1"
-          display="none"
-          display_tablet="inline"
-        />
-        <Circle
-          color="yellow"
-          width="21px"
-          height="21px"
-          top="10px"
-          right="320px"
-          zIndex="1"
-          display="none"
-          display_tablet="inline"
-        />
-        <Circle
-          color="blue"
-          width="57px"
-          height="57px"
-          top="32px"
-          right="61px"
-          display="none"
-          display_tablet="inline"
-        />
-        <Circle
-          color="blue"
-          width="15px"
-          height="15px"
-          top="92px"
-          left="9px"
-          zIndex="1"
-          display="inline"
-          display_tablet="none"
-        />
-        <Circle
-          color="white"
-          width="15px"
-          height="15px"
-          top="130px"
-          left="10px"
-          zIndex="1"
-          display="inline"
-          display_tablet="none"
-        />
-        <Circle
-          color="darkGray"
-          width="15px"
-          height="15px"
-          top="195px"
-          left="5px"
-          zIndex="1"
-          display="inline"
-          display_tablet="none"
-        />
-        <Div display="block">
-          <H2
-            type="h2"
-            textAlign="center"
-            textAlign_tablet="center"
-            fontSize="40px"
-            fontSize_tablet="50px"
-            lineHeight="38px"
-            lineHeight_tablet="60px"
-            margin_tablet="40px 0 40px 0"
-          >
-            {yml.header.title}
-          </H2>
-          <Paragraph
-            color="black"
-            opacity="1"
-            margin="20px 0"
-            padding="0"
-            width="auto"
-            letterSpacing="0.05em"
-            textAlign="center"
-            textAlign_tablet="center"
-            fontSize="24px"
-            lineHeight="28px"
-          >
-            {yml.header.paragraph}
-          </Paragraph>
+        <GridContainerWithImage
+          padding="0"
+          columns_tablet="2"
+          margin="0 auto"
+          position="relative"
+          gridColumn_tablet="1/15"
+          maxWidth="1280px"
+          justifyContent="center"
+        >
           <Div
-            flexDirection_tablet="row"
+            display="flex"
             flexDirection="column"
-            justifyContent="start"
-            justifyContent_tablet="center"
             alignItems="center"
-            margin_tablet="0 0 50px 0"
+            alignItems_tablet="left"
+            margin="auto"
+            padding_tablet="0"
           >
-            <Div width="100%" width_tablet="fit-content">
-              <Link to={yml.button.apply_button_link} style={{ width: "100%" }}>
-                <Button
-                  variant="full"
-                  justifyContent="center"
-                  width="100%"
-                  width_tablet="fit-content"
-                  color={Colors.black}
-                  margin_tablet="10px 24px 10px 0"
-                  textColor="white"
+            <H1
+              type="h1"
+              textAlign="center"
+              textAlign_tablet="left"
+              fontSize="30px"
+              fontSize_md="60px"
+              lineHeight="30px"
+              lineHeight_md="65.28px"
+              fontWeight="400"
+              margin_tablet="0"
+              fontFamily="Archivo,Lato,sans-serif"
+            >
+              {yml.header.title}
+            </H1>
+            <Paragraph
+              textAlign="center"
+              textAlign_tablet="left"
+              textAlign_md="left"
+              textAlign_xl="left"
+              fontFamily="Lato,sans-serif"
+              color="black"
+              opacity="1"
+              margin="20px 0"
+              padding="0"
+              letterSpacing="0.05em"
+              fontSize="15px"
+              fontSize_md="24px"
+              lineHeight="28.8px"
+            >
+              {yml.header.paragraph}
+            </Paragraph>
+            <Div
+              flexDirection_tablet="row"
+              flexDirection="column"
+              justifyContent="left"
+              alignItems="center"
+              margin_tablet="0"
+              width="100%"
+            >
+              <Div width="100%" width_tablet="fit-content">
+                <Link
+                  to={yml.button.apply_button_link}
+                  style={{ width: "100%" }}
                 >
-                  {applyButtonText}
-                </Button>
-              </Link>
-            </Div>
-            <Div width="100%" width_tablet="fit-content">
-              <Link to="#fake_bottom" style={{ width: "100%" }}>
-                <Button
-                  display="block"
-                  width="100%"
-                  width_tablet="fit-content"
-                  variant="outline"
-                  color={Colors.black}
-                  margin="10px 0 50px 0"
-                  margin_tablet="0"
-                  textColor={Colors.black}
-                  textAlign="center"
-                >
-                  {yml.button.btn_label}
-                </Button>
-              </Link>
+                  <Button
+                    variant="full"
+                    justifyContent="center"
+                    width="100%"
+                    width_tablet="fit-content"
+                    color={Colors.black}
+                    margin_tablet="10px 24px 10px 0"
+                    textColor="white"
+                  >
+                    {applyButtonText}
+                  </Button>
+                </Link>
+              </Div>
+              <Div width="100%" width_tablet="fit-content">
+                <Link to="#fake_bottom" style={{ width: "100%" }}>
+                  <Button
+                    padding="12px"
+                    padding_tablet="12px"
+                    display="block"
+                    width="100%"
+                    width_tablet="fit-content"
+                    variant="outline"
+                    color={Colors.black}
+                    margin="10px 0 10px 0"
+                    margin_tablet="0"
+                    textColor={Colors.black}
+                  >
+                    {yml.button.btn_label}
+                  </Button>
+                </Link>
+              </Div>
             </Div>
           </Div>
-        </Div>
+          <Div
+            display="flex"
+            justifyContent="center"
+            height="auto"
+            width="100%"
+            padding="0"
+          >
+            <StyledBackgroundSection
+              display_tablet="block"
+              height="180px"
+              height_tablet="382px"
+              width="100%"
+              image={
+                yml.header.image &&
+                yml.header.image.childImageSharp.gatsbyImageData
+              }
+              alt="Hero image"
+              bgSize="contain"
+            />
+          </Div>
+        </GridContainerWithImage>
       </Div>
       <OurPartners margin="0" images={partnersData.partners.images} marquee />
       <ScholarshipProjects
@@ -412,6 +282,17 @@ export const query = graphql`
           header {
             title
             paragraph
+            image {
+              childImageSharp {
+                gatsbyImageData(
+                  layout: CONSTRAINED # --> CONSTRAINED || FIXED || FULL_WIDTH
+                  width: 700
+                  quality: 100
+                  placeholder: NONE # --> NONE || DOMINANT_COLOR || BLURRED | TRACED_SVG
+                  breakpoints: [200, 340, 520, 890]
+                )
+              }
+            }
           }
           button {
             btn_label
