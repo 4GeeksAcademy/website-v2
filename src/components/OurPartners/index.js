996987d95794676e223c439c628664c82ec54c91
gustavomm19 <gustavomm199@gmail.com>
2024-05-07 01:29:15 +0000

    add breakpoints to partners carousel

--- src/components/OurPartners/index.js
+++ src/components/OurPartners/index.js
@@ -284,6 +284,43 @@ const OurPartners = ({
         customSettings={{
           slidesToShow: 4,
           slidesToScroll: 4,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true,
+              },
+            },
+            {
+              breakpoint: 780,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2,
+                dots: false,
+              },
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2,
+                dots: false,
+              },
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+              },
+            },
+          ],
         }}
       >
         {images.map((elem) => {
