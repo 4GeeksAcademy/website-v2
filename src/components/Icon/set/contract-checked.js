950952737784a06c2528dfc1da33b70dff47bd9a
Gustavo L <gust.20220@gmail.com>
2024-09-16 17:38:26 +0000

    icons update in programs

--- src/components/Icon/set/contract-checked.js
+++ src/components/Icon/set/contract-checked.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default (props) => (
-  <svg
+<svg
     style={props.style}
     width={props.width || "24"}
     height={props.height || "24"}
@@ -11,67 +11,67 @@ export default (props) => (
   >
     <path
       d="M17.5 23.5C20.8137 23.5 23.5 20.8137 23.5 17.5C23.5 14.1863 20.8137 11.5 17.5 11.5C14.1863 11.5 11.5 14.1863 11.5 17.5C11.5 20.8137 14.1863 23.5 17.5 23.5Z"
-      stroke="#A4A4A4"
+      stroke="#0084ff"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
       d="M20.5 15.5L16.5 19.5L14.5 17.5"
-      stroke="#A4A4A4"
+      stroke="#0084ff"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
       d="M11 23.5H0.5V2.5H2.5"
-      stroke="#A4A4A4"
+      stroke="#0084ff"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
       d="M16.5 2.5H18.5V9.5"
-      stroke="#A4A4A4"
+      stroke="#0084ff"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
       d="M11.5 2.5C11.5 1.96957 11.2893 1.46089 10.9142 1.08582C10.5391 0.710743 10.0304 0.5 9.5 0.5C8.96957 0.5 8.46089 0.710743 8.08582 1.08582C7.71074 1.46089 7.5 1.96957 7.5 2.5H4.5V5.5H14.5V2.5H11.5Z"
-      stroke="#A4A4A4"
+      stroke="#0084ff"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
       d="M16.5 9.5V4.5H14.5"
-      stroke="#A4A4A4"
+      stroke="#0084ff"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
       d="M4.5 4.5H2.5V21.5H10.5"
-      stroke="#A4A4A4"
+      stroke="#0084ff"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
       d="M5.5 9.5H13.5"
-      stroke="#A4A4A4"
+      stroke="#0084ff"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
       d="M5.5 11.5H12"
-      stroke="#A4A4A4"
+      stroke="#0084ff"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
       d="M5.5 13.5H10.5"
-      stroke="#A4A4A4"
+      stroke="#0084ff"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
       d="M5.5 15.5H10"
-      stroke="#A4A4A4"
+      stroke="#0084ff"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
