a53a6be664e91c4b60977008abbebde21cd2770d
Gustavo L <gust.20220@gmail.com>
2025-03-13 18:55:39 +0000

    build

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Devices } from "../Responsive";
 
-// background 
+// background
 const Overlay = styled.div`
   position: fixed;
   top: 0;
